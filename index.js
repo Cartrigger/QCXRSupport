1c309fad3914bf5b9c567200ac5de6b33fd5e5ac
Cart <cartrigger@gmail.com>
2022-10-15 18:33:31 -0400

    first part
    yoo

---
+++ index.js
@@ -0,0 +1,25 @@
+
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+
+client.once('ready', () => {
+    console.log('Your mom');
+});
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    client.commands.set(command.data.name, command);
+
+client.login(token);
+}
+
