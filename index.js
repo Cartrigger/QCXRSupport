24ab98ee828d6d6c3e7f008f6ecbb30012a1f0fe
Cart <cartrigger@gmail.com>
2022-10-15 23:48:38 -0400

    CAN YOU WORK

--- index.js
+++ index.js
@@ -59,8 +59,7 @@ process.stdin.setRawMode(true);
 process.stdin.on("keypress", (char, evt) => {
     console.log("=====Key pressed=====");
     //console.log("Char:", JSON.stringify(char), "Evt:", JSON.stringify(evt));
-    if (char === "q") process.exit();
-    if (char === "q") process.on('exit', () => {const child = spawn('node', ['remove-commands.js'], {
+    if (char === "q") process.exit(); process.on('exit', () => {const child = spawn('node', ['remove-commands.js'], {
         detached: true,
         stdio: 'ignore',
 });
