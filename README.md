3ff59f0a8fdc3459ab04af314c625f59333a7a9b
Cart <cartrigger@gmail.com>
2022-10-15 21:45:49 -0400

    New README (for setup)
    Also having remove-commands.js a thing so then you dont have to be stupid like i was
    removes comments from deploy-commands.js

--- README.md
+++ README.md
@@ -3,3 +3,13 @@ A shitty bot made in JS
 Atleast this doesn't suck the most.
 
 You can see I suck at this and shouldn't be doing this :>
+
+
+How to setup this bot:
+First: Set config.json to your information.
+Second: Run (node deploy-commands.js)
+Final: Run index.js
+
+To remove commands:
+Get command ID's
+Remove it by putting command ID's in 'YOURCMDIDHERE' in remove-commands.js
--- deploy-commands.js
+++ deploy-commands.js
@@ -20,15 +20,3 @@ rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
     .then(() => console.log('Successfully registered application commands.'))
     .catch(console.error);
 
-
-//old code, if want to use, move to top below the const's :lol:
-/*
-const rest = new REST({ version: '10' }).setToken(token);
-//keep this for deleting cmds
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-    .then((data) => console.log(`Successfully registered ${data.length} application commands.`))
-    .catch(console.error)
-
-rest.delete(Routes.applicationGuildCommand(clientId, guildId, '1030947441027076196'))
-    .then(() => console.log('Successfully deleted guild command'))
-    .catch(console.error);*/
