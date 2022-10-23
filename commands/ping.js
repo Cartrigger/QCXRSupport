826c8df88024ede1f756e4ed0136ea88fb5eca9c
Cart <cartrigger@gmail.com>
2022-10-23 01:15:19 -0400

    stop spam and work on crafty integration

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,8 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
-        .setDescription('Ping SMH'),
+        .setDescription('Ping SMH')
+        .setDefaultPermissions(ChangeNickname),
     async execute(interaction) {
         await interaction.reply('Pong!');
     },
--- commands/why.js
+++ commands/why.js
@@ -3,7 +3,8 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('why')
-        .setDescription('Tells you why QC is broken'),
+        .setDescription('Tells you why QC is broken')
+        .setDefaultPermissions(ChangeNickname),
     async execute(interaction) {
         await interaction.reply('Questcraft is broken due to v44 breaking the way we started OpenXR (2d - 3d)');
     },
