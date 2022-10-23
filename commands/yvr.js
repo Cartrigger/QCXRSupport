f7e6c65209e18f6c4b305516af1484d66ba5c733
Cart <81428538+Cartrigger@users.noreply.github.com>
2022-10-22 20:39:30 -0400

    Update yvr.js

--- commands/yvr.js
+++ commands/yvr.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
       data: new SlashCommandBuilder()
-          .setName('YVR')
+          .setName('yvr')
           .setDescription('Tells you what YVR is.'),
       async execute(interaction) {
                 await interaction.reply('YVR is a Chinese headset company. They have requested YVRCraft. We are working on both QCXR and YVRCraft.');
