d52b5c8e49c6b7376ff7a06348a4b3f0ee85e63d
Cart <cartrigger@gmail.com>
2022-10-23 02:14:23 -0400

    a

--- commands/why.js
+++ commands/why.js
@@ -5,6 +5,6 @@ module.exports = {
         .setName('why')
         .setDescription('Tells you why QC is broken'),
     async execute(interaction) {
-        await interaction.reply('Questcraft is broken due to v44 breaking the way we started OpenXR (2d - 3d)');
+        await interaction.reply('Questcraft is broken due to v44 breaking the way we started OpenXR (2d --> 3d)');
     },
 };
