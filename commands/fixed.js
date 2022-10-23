5dc8d703d22b46d9dac3a1333733e1662acb2a18
Cart <cartrigger@gmail.com>
2022-10-23 01:51:01 -0400

    Revert "augh"
    This reverts commit 8e46fba97143286b5ef172d1cfb44648bc707d2b.

--- commands/fixed.js
+++ commands/fixed.js
@@ -1,11 +1,10 @@
-const { PermissionFlagsBits } = require('discord.js');
-const { SlashCommandBuilder, Client, ReactionUserManager, Routes } = require('discord.js');
-import { PermissionFlagsBits } from 'discord.js'
+const { SlashCommandBuilder } = require('discord.js');
+
 module.exports = {
       data: new SlashCommandBuilder()
           .setName('fixed')
           .setDescription('Is it fixed?')
-        .setDefaultPermissions(PermissionFlagsBits.ChangeNickname),
+          .setDefaultPermissions(ChangeNickname),
       async execute(interaction) {
                 await interaction.reply('**no**, you will get an @ everyone when it gets done.');
       },
--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,10 @@
-const { PermissionFlagsBits } = require('discord.js');
-const { SlashCommandBuilder, Client, ReactionUserManager, Routes } = require('discord.js');
-import { PermissionFlagsBits } from 'discord.js'
+const { SlashCommandBuilder } = require('discord.js');
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Ping SMH')
-        .setDefaultPermissions(PermissionFlagsBits.ChangeNickname),
+        .setDefaultPermissions(ChangeNickname),
     async execute(interaction) {
         await interaction.reply('Pong!');
     },
