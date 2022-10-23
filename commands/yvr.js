59ce0c448a74482729002e6db9d3ddaa03b4b330
Cart <cartrigger@gmail.com>
2022-10-23 01:50:52 -0400

    Revert "fix or else i will slap you"
    This reverts commit b2bbaab972a0eafe4cfa60d1ca1c677b689550ae.

--- commands/yvr.js
+++ commands/yvr.js
@@ -1,12 +1,11 @@
 const { PermissionFlagsBits } = require('discord.js');
-const { PermissionsBitField } = require('discord.js');
 const { SlashCommandBuilder, Client, ReactionUserManager, Routes } = require('discord.js');
 
 module.exports = {
           data: new SlashCommandBuilder()
           .setName('yvr')
           .setDescription('Tells you what YVR is.')
-          .setDefaultPermissions(PermissionsBitField.ChangeNickname),
+          .setDefaultPermissions(PermissionFlagBits.ChangeNickname),
           async execute(interaction) {
                 await interaction.reply('YVR is a Chinese headset company. They have requested YVRCraft. We are working on both QCXR and YVRCraft.');
       },
