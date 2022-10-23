57dd76193b556d6b0c25a71cbef77232fde0984b
Cart <cartrigger@gmail.com>
2022-10-23 16:31:37 -0400

    less errors

--- index.ts
+++ index.ts
@@ -1,14 +1,10 @@
 import dotenv from 'dotenv';
 dotenv.config()
-import DiscordJS, { Intents, Message, MessageEmbed } from 'discord.js';
-const { REST } = require('@discordjs/rest')
-const { Routes } = require('discord.js')
-const client = new DiscordJS.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+import DiscordJS, { Intents, Message, MessageEmbed } from 'discord.js13';
 const config = require('./ptsd')
 const prefix = config.prefix
 const replyMessage = config.replyMessage
 const fs = require('node:fs');
-const rest = new REST({ version: '10' }).setToken('token');
 
 const addserversEmbed = new MessageEmbed()
   .setColor(config.addserversEmbed.color)
