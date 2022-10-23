ba3279c8dc3d02c662b36f18dac7b92ef5bf7761
Cart <81428538+Cartrigger@users.noreply.github.com>
2022-10-23 00:15:24 -0400

    Update fixed.js

--- commands/fixed.js
+++ commands/fixed.js
@@ -5,6 +5,6 @@ module.exports = {
           .setName('fixed')
           .setDescription('Is it fixed?'),
       async execute(interaction) {
-                await interaction.reply('**no**');
+                await interaction.reply('**no**, you will get an @ everyone when it gets done.');
       },
 };
