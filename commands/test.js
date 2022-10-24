012f98e37589b7d1dd65369911f7410ec773cebe
Cart <81428538+Cartrigger@users.noreply.github.com>
2022-10-23 23:24:44 -0400

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('aweroiuhterwiturehtreiuhotreiohute')
+        .setName('aweroiuhterwiturehtreiuhotreiohut')
         .setDescription('something'),
     async execute(interaction) {
         await interaction.reply('test');
