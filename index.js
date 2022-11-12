fb0f0911a6ed343e3c5957e3abbd42ff314d1e54
Cart <cartrigger@gmail.com>
2022-11-12 03:26:46 -0500

    crafty code go brrrrrrrrr

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord.js');
 const { clientId, guildId, token } = require('./config.json');
 const spawn = require('child_process').spawn;
+const prefix = config.prefix
 
 const commands = [];
 
@@ -13,6 +14,17 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+const qcfix = new MessageEmbed()
+    .setColor(config.addserversEmbed.color)
+    .setTitle(config.addserversEmbed.title)
+    .setDescription(config.addserversEmbed.description)
+    .setThumbnail(config.addserversEmbed.thumbnail)
+    .addFields(
+        { name: config.addserversEmbed.fields[0][0], value: config.addserversEmbed.fields[0][1] },
+    )
+    .setTimestamp()
+    .setFooter({ text: config.addserversEmbed.footer.text, iconURL: config.addserversEmbed.footer.iconURL });
 
 for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
@@ -45,6 +57,19 @@ client.on('interactionCreate', async interaction => {
     } catch (error) {
         console.error(error);
         await interaction.reply({ content: 'There was an error while executing this command! Please contact Cart.', ephemeral: true });
+    }
+});
+
+client.on(ClientEvents.messageCreate, (message) => {
+    if (message.author.bot) return;
+    let msg = message.content.toLowerCase()
+    if (msg.startsWith(prefix)) {
+        const args = msg.slice(prefix.length).split(/ +/);
+        const command = args.shift().toLowerCase();
+
+        if (command === 'is it fixed?') {
+            message.channel.send({ embeds: [qcfix] })
+}
     }
 });
 
