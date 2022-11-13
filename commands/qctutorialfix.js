f2a6a84db42417ee001fe7f82abc6018efe39d1b
Cart <cartrigger@gmail.com>
2022-11-13 16:25:58 -0500

    Create qctutorialfix.js

---
+++ commands/qctutorialfix.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('qctutorial')
+        .setDescription('Tutorial fix.'),
+    async execute(interaction) {
+        await interaction.reply('This video is not endorsed by the QC team, but made by my creator. https://www.youtube.com/watch?v=TLWR_3hEBJU ');
+    },
+};
