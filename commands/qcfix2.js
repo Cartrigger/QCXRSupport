a01f2852fcd799a2cea0e4db2625d632617b37d7
Cart <cartrigger@gmail.com>
2022-11-13 17:07:15 -0500

    Update qcfix2.js

--- commands/qcfix2.js
+++ commands/qcfix2.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('qctutorial2')
-        .setDescription('Tutorial fix.'),
+        .setDescription('Tutorial fix2.'),
     async execute(interaction) {
         await interaction.reply('This is the second tutorial if you dont understand the first. https://youtu.be/gP5Kn9ddI2U');
     },
