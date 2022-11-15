fbad7f16bdc92d3b750fd7aa826179fb9de4a77a
Cart <81428538+Cartrigger@users.noreply.github.com>
2022-11-15 00:43:58 -0500

    Update qctutorialfix.js

--- commands/qctutorialfix.js
+++ commands/qctutorialfix.js
@@ -5,6 +5,6 @@ module.exports = {
         .setName('qctutorial')
         .setDescription('Tutorial fix.'),
     async execute(interaction) {
-        await interaction.reply('This video is not endorsed by the QC team, but made by my creator. https://www.youtube.com/watch?v=TLWR_3hEBJU');
+        await interaction.reply('This video is not endorsed by the QC team, but made by my creator. https://www.youtube.com/watch?v=TLWR_3hEBJU You will not get support, but it works.');
     },
 };
