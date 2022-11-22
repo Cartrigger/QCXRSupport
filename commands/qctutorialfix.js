fda860f62d1bf72dbc129ecbbeedb0dd02ced95f
Cart <81428538+Cartrigger@users.noreply.github.com>
2022-11-22 01:32:59 -0500

    Update qctutorialfix.js

--- commands/qctutorialfix.js
+++ commands/qctutorialfix.js
@@ -5,6 +5,6 @@ module.exports = {
         .setName('qctutorial')
         .setDescription('Tutorial fix.'),
     async execute(interaction) {
-        await interaction.reply('This video is not endorsed by the QC team, but made by my creator. https://www.youtube.com/watch?v=TLWR_3hEBJU You will not get support, but it works.');
+        await interaction.reply('This video is not endorsed by the QC team, but made by my creator. https://www.youtube.com/watch?v=2TYeNhm_Jn8 You will not get support, but it works.');
     },
 };
