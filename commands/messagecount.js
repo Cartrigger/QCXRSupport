51d8d3a9899cbbdbc0fb82fec72ba3bacd478e0b
Cart <cartrigger@gmail.com>
2022-11-23 23:56:09 -0500

    Create messagecount.js

---
+++ commands/messagecount.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+async function fetchAllMessages() {
+    const channel = client.channels.cache.get("820767484042018832");
+    let messages = [];
+    let message = await channel.messages
+        .fetch({ limit: 1 })
+        .then(messagePage => (messagePage.size === 1 ? messagePage.at(0) : null));
+
+    while (message) {
+        await channel.messages
+            .fetch({ limit: 100, before: message.id })
+            .then(messagePage => {
+                messagePage.forEach(msg => messages.push(msg));
+
+                // Update our message pointer to be last message in page of messages
+                message = 0 < messagePage.size ? messagePage.at(messagePage.size - 1) : null;
+            },
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('messagefetcher')
+        .setDescription('Fetches messages'),
+    async execute(interaction) {
+        await interaction.reply('Command not ready. Coming soon.');
+    },
+})}};
