a3354ba7319a620be000980f00f67448fde1efac
Cart <cartrigger@gmail.com>
2022-11-23 22:09:28 -0500

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('Github')
+        .setName('github')
         .setDescription('Tells you the github'),
     async execute(interaction) {
         await interaction.reply('https://github.com/Cartrigger/QCXRSoonBot');
