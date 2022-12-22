c46e355463dd8a170053369923c0f0fef088c018
The Judge <53906078+thejudge156@users.noreply.github.com>
2022-12-21 22:14:36 -0800

    xmas time

---
+++ commands/xmas.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('xmas')
+        .setDescription('MERRY CHRYSLER'),
+    async execute(interaction) {
+        let date = new Date(2022, 12, 25);
+        let currDate = Date.now();
+        let seconds = (currDate - date)/1000 % 60;
+        let minutes = (currDate - date)/(1000*60) % 60;
+        let hours = (currDate - date)/(1000*60*60) % 24;
+        await interaction.reply('There is %s seconds, %s minutes, and %s hours till Christmas!', seconds, minutes, hours);
+    },
+};
