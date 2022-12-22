6cb88277323f7dbac029a3d36fc87bdba81cfb96
The Judge <53906078+thejudge156@users.noreply.github.com>
2022-12-21 22:21:45 -0800

    WHOOPS *bot be time traveling*

--- commands/xmas.js
+++ commands/xmas.js
@@ -7,9 +7,9 @@ module.exports = {
     async execute(interaction) {
         let date = new Date(2022, 12, 25);
         let currDate = Date.now();
-        let seconds = (currDate - date)/1000 % 60;
-        let minutes = (currDate - date)/(1000*60) % 60;
-        let hours = (currDate - date)/(1000*60*60) % 24;
+        let seconds = (date - currDate)/1000 % 60;
+        let minutes = (date - currDate)/(1000*60) % 60;
+        let hours = (date - currDate)/(1000*60*60) % 24;
         await interaction.reply('There is ' + seconds + ' seconds, ' + minutes + ' minutes, and ' + hours + ' hours till Christmas!');
     },
 };
