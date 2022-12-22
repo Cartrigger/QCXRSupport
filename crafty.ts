bb860257eeb09daacc6727af40278d43d8818317
Cart <cartrigger@gmail.com>
2022-12-22 01:09:53 -0500

    Update crafty.ts

--- crafty.ts
+++ crafty.ts
@@ -1,152 +1,147 @@
 const path = require('node:path');
-const { tokens } = require('dotenv');
-tokens()
-const config = require('./ptsd')
-const prefix = config.prefix
-const replyMessage = config.replyMessage
+const { config } = require('dotenv');
+config()
+const crafty = require('./ptsd')
+const prefix = crafty.prefix
+const replyMessage = crafty.replyMessage
 const fs = require('node:fs');
-const { client, ClientEvents, Intents, Message } = require ('discord.js13');
+const { client, ClientEvents, Intents, Message } = require ('discord.js');
 const { EmbedBuilder } = require('discord.js');
 const token = process.env.token;
 const clientId = process.env.clientID;
 const guildId = process.env.guildID;
 
 const addserversEmbed = new EmbedBuilder()
-  .setColor(config.addserversEmbed.color)
-  .setTitle(config.addserversEmbed.title)
-  .setDescription(config.addserversEmbed.description)
-  .setThumbnail(config.addserversEmbed.thumbnail)
+  .setColor(crafty.addserversEmbed.color)
+  .setTitle(crafty.addserversEmbed.title)
+  .setDescription(crafty.addserversEmbed.description)
+  .setThumbnail(crafty.addserversEmbed.thumbnail)
   .addFields(
-    { name: config.addserversEmbed.fields[0][0], value: config.addserversEmbed.fields[0][1] },
+    { name: crafty.addserversEmbed.fields[0][0], value: crafty.addserversEmbed.fields[0][1] },
   )
   .setTimestamp()
-  .setFooter({ text: config.addserversEmbed.footer.text, iconURL: config.addserversEmbed.footer.iconURL });
+  .setFooter({ text: crafty.addserversEmbed.footer.text, iconURL: crafty.addserversEmbed.footer.iconURL });
 
 const createServersEmbed = new EmbedBuilder()
-  .setColor(config.createServersEmbed.color)
-  .setTitle(config.createServersEmbed.title)
-  .setDescription(config.createServersEmbed.description)
-  .setThumbnail(config.createServersEmbed.thumbnail)
+  .setColor(crafty.createServersEmbed.color)
+  .setTitle(crafty.createServersEmbed.title)
+  .setDescription(crafty.createServersEmbed.description)
+  .setThumbnail(crafty.createServersEmbed.thumbnail)
   .addFields(
-    { name: config.createServersEmbed.fields[0][0], value: config.createServersEmbed.fields[0][1] },
+    { name: crafty.createServersEmbed.fields[0][0], value: crafty.createServersEmbed.fields[0][1] },
   )
   .setTimestamp()
-  .setFooter({ text: config.createServersEmbed.footer.text, iconURL: config.createServersEmbed.footer.iconURL });
+  .setFooter({ text: crafty.createServersEmbed.footer.text, iconURL: crafty.createServersEmbed.footer.iconURL });
 
 const howModsEmbed = new EmbedBuilder()
-  .setColor(config.howModsEmbed.color)
-  .setTitle(config.howModsEmbed.title)
-  .setDescription(config.howModsEmbed.description)
-  .setThumbnail(config.howModsEmbed.thumbnail)
+  .setColor(crafty.howModsEmbed.color)
+  .setTitle(crafty.howModsEmbed.title)
+  .setDescription(crafty.howModsEmbed.description)
+  .setThumbnail(crafty.howModsEmbed.thumbnail)
   .addFields(
-    { name: config.howModsEmbed.fields[0][0], value: config.howModsEmbed.fields[0][1] },
-    { name: config.howModsEmbed.fields[1][0], value: config.howModsEmbed.fields[1][1] },
-    { name: config.howModsEmbed.fields[2][0], value: config.howModsEmbed.fields[2][1] },
-    { name: config.howModsEmbed.fields[3][0], value: config.howModsEmbed.fields[3][1] },
+    { name: crafty.howModsEmbed.fields[0][0], value: crafty.howModsEmbed.fields[0][1] },
+    { name: crafty.howModsEmbed.fields[1][0], value: crafty.howModsEmbed.fields[1][1] },
+    { name: crafty.howModsEmbed.fields[2][0], value: crafty.howModsEmbed.fields[2][1] },
+    { name: crafty.howModsEmbed.fields[3][0], value: crafty.howModsEmbed.fields[3][1] },
   )
   .setTimestamp()
-  .setFooter({ text: config.howModsEmbed.footer.text, iconURL: config.howModsEmbed.footer.iconURL });
+  .setFooter({ text: crafty.howModsEmbed.footer.text, iconURL: crafty.howModsEmbed.footer.iconURL });
 
 const addResourcePacksEmbed = new EmbedBuilder()
-  .setColor(config.addResourcePacksEmbed.color)
-  .setTitle(config.addResourcePacksEmbed.title)
-  .setDescription(config.addResourcePacksEmbed.description)
-  .setThumbnail(config.addResourcePacksEmbed.thumbnail)
+  .setColor(crafty.addResourcePacksEmbed.color)
+  .setTitle(crafty.addResourcePacksEmbed.title)
+  .setDescription(crafty.addResourcePacksEmbed.description)
+  .setThumbnail(crafty.addResourcePacksEmbed.thumbnail)
   .addFields(
-    { name: config.addResourcePacksEmbed.fields[0][0], value: config.addResourcePacksEmbed.fields[0][1] },
-    { name: config.addResourcePacksEmbed.fields[1][0], value: config.addResourcePacksEmbed.fields[1][1] },
-    { name: config.addResourcePacksEmbed.fields[2][0], value: config.addResourcePacksEmbed.fields[2][1] },
-    { name: config.addResourcePacksEmbed.fields[3][0], value: config.addResourcePacksEmbed.fields[3][1] }
+    { name: crafty.addResourcePacksEmbed.fields[0][0], value: crafty.addResourcePacksEmbed.fields[0][1] },
+    { name: crafty.addResourcePacksEmbed.fields[1][0], value: crafty.addResourcePacksEmbed.fields[1][1] },
+    { name: crafty.addResourcePacksEmbed.fields[2][0], value: crafty.addResourcePacksEmbed.fields[2][1] },
+    { name: crafty.addResourcePacksEmbed.fields[3][0], value: crafty.addResourcePacksEmbed.fields[3][1] }
   )
   .setTimestamp()
-  .setFooter({ text: config.addResourcePacksEmbed.footer.text, iconURL: config.addResourcePacksEmbed.footer.iconURL });
+  .setFooter({ text: crafty.addResourcePacksEmbed.footer.text, iconURL: crafty.addResourcePacksEmbed.footer.iconURL });
 
 const fixBlackScreenEmbed = new EmbedBuilder()
-  .setColor(config.fixBlackScreenEmbed.color)
-  .setTitle(config.fixBlackScreenEmbed.title)
-  .setDescription(config.fixBlackScreenEmbed.description)
-  .setThumbnail(config.fixBlackScreenEmbed.thumbnail)
+  .setColor(crafty.fixBlackScreenEmbed.color)
+  .setTitle(crafty.fixBlackScreenEmbed.title)
+  .setDescription(crafty.fixBlackScreenEmbed.description)
+  .setThumbnail(crafty.fixBlackScreenEmbed.thumbnail)
   .addFields(
-    { name: config.fixBlackScreenEmbed.fields[0][0], value: config.fixBlackScreenEmbed.fields[0][1] },
-    { name: config.fixBlackScreenEmbed.fields[1][0], value: config.fixBlackScreenEmbed.fields[1][1] },
-    { name: config.fixBlackScreenEmbed.fields[2][0], value: config.fixBlackScreenEmbed.fields[2][1] },
-    { name: config.fixBlackScreenEmbed.fields[3][0], value: config.fixBlackScreenEmbed.fields[3][1] },
+    { name: crafty.fixBlackScreenEmbed.fields[0][0], value: crafty.fixBlackScreenEmbed.fields[0][1] },
+    { name: crafty.fixBlackScreenEmbed.fields[1][0], value: crafty.fixBlackScreenEmbed.fields[1][1] },
+    { name: crafty.fixBlackScreenEmbed.fields[2][0], value: crafty.fixBlackScreenEmbed.fields[2][1] },
+    { name: crafty.fixBlackScreenEmbed.fields[3][0], value: crafty.fixBlackScreenEmbed.fields[3][1] },
   )
   .setTimestamp()
-  .setFooter({ text: config.fixBlackScreenEmbed.footer.text, iconURL: config.fixBlackScreenEmbed.footer.iconURL });
+  .setFooter({ text: crafty.fixBlackScreenEmbed.footer.text, iconURL: crafty.fixBlackScreenEmbed.footer.iconURL });
 
 const commandsEmbed = new EmbedBuilder()
-  .setColor(config.commandsEmbed.color)
-  .setTitle(config.commandsEmbed.title)
-  .setDescription(config.commandsEmbed.description)
-  .setThumbnail(config.commandsEmbed.thumbnail)
+  .setColor(crafty.commandsEmbed.color)
+  .setTitle(crafty.commandsEmbed.title)
+  .setDescription(crafty.commandsEmbed.description)
+  .setThumbnail(crafty.commandsEmbed.thumbnail)
   .addFields(
-    { name: config.commandsEmbed.fields[0][0], value: config.commandsEmbed.fields[0][1] },
-    { name: config.commandsEmbed.fields[1][0], value: config.commandsEmbed.fields[1][1] },
-    { name: config.commandsEmbed.fields[2][0], value: config.commandsEmbed.fields[2][1] },
-    { name: config.commandsEmbed.fields[3][0], value: config.commandsEmbed.fields[3][1] },
-    { name: config.commandsEmbed.fields[4][0], value: config.commandsEmbed.fields[4][1] },
-    { name: config.commandsEmbed.fields[5][0], value: config.commandsEmbed.fields[5][1] },
-    { name: config.commandsEmbed.fields[6][0], value: config.commandsEmbed.fields[6][1] },
-    { name: config.commandsEmbed.fields[7][0], value: config.commandsEmbed.fields[7][1] },
-    { name: config.commandsEmbed.fields[8][0], value: config.commandsEmbed.fields[8][1] },
+    { name: crafty.commandsEmbed.fields[0][0], value: crafty.commandsEmbed.fields[0][1] },
+    { name: crafty.commandsEmbed.fields[1][0], value: crafty.commandsEmbed.fields[1][1] },
+    { name: crafty.commandsEmbed.fields[2][0], value: crafty.commandsEmbed.fields[2][1] },
+    { name: crafty.commandsEmbed.fields[3][0], value: crafty.commandsEmbed.fields[3][1] },
+    { name: crafty.commandsEmbed.fields[4][0], value: crafty.commandsEmbed.fields[4][1] },
+    { name: crafty.commandsEmbed.fields[5][0], value: crafty.commandsEmbed.fields[5][1] },
+    { name: crafty.commandsEmbed.fields[6][0], value: crafty.commandsEmbed.fields[6][1] },
+    { name: crafty.commandsEmbed.fields[7][0], value: crafty.commandsEmbed.fields[7][1] },
+    { name: crafty.commandsEmbed.fields[8][0], value: crafty.commandsEmbed.fields[8][1] },
   )
   .setTimestamp()
-  .setFooter({ text: config.commandsEmbed.footer.text, iconURL: config.commandsEmbed.footer.iconURL });
+  .setFooter({ text: crafty.commandsEmbed.footer.text, iconURL: crafty.commandsEmbed.footer.iconURL });
 
 
 const installQCEmbed = new EmbedBuilder()
-  .setColor(config.installQCEmbed.color)
-  .setTitle(config.installQCEmbed.title)
-  .setDescription(config.installQCEmbed.description)
-  .setThumbnail(config.installQCEmbed.thumbnail)
+  .setColor(crafty.installQCEmbed.color)
+  .setTitle(crafty.installQCEmbed.title)
+  .setDescription(crafty.installQCEmbed.description)
+  .setThumbnail(crafty.installQCEmbed.thumbnail)
   .addFields(
-    { name: config.installQCEmbed.fields[0][0], value: config.installQCEmbed.fields[0][1] },
-    { name: config.installQCEmbed.fields[1][0], value: config.installQCEmbed.fields[1][1] }
+    { name: crafty.installQCEmbed.fields[0][0], value: crafty.installQCEmbed.fields[0][1] },
+    { name: crafty.installQCEmbed.fields[1][0], value: crafty.installQCEmbed.fields[1][1] }
   )
   .setTimestamp()
-  .setFooter({ text: config.installQCEmbed.footer.text, iconURL: config.installQCEmbed.footer.iconURL });
+  .setFooter({ text: crafty.installQCEmbed.footer.text, iconURL: crafty.installQCEmbed.footer.iconURL });
 
 const notshowingupEmbed = new EmbedBuilder()
-  .setColor(config.notshowingupEmbed.color)
-  .setTitle(config.notshowingupEmbed.title)
-  .setDescription(config.notshowingupEmbed.description)
-  .setThumbnail(config.notshowingupEmbed.thumbnail)
+  .setColor(crafty.notshowingupEmbed.color)
+  .setTitle(crafty.notshowingupEmbed.title)
+  .setDescription(crafty.notshowingupEmbed.description)
+  .setThumbnail(crafty.notshowingupEmbed.thumbnail)
   .addFields(
-    { name: config.notshowingupEmbed.fields[0][0], value: config.notshowingupEmbed.fields[0][1] },
-    { name: config.notshowingupEmbed.fields[1][0], value: config.notshowingupEmbed.fields[1][1] }
+    { name: crafty.notshowingupEmbed.fields[0][0], value: crafty.notshowingupEmbed.fields[0][1] },
+    { name: crafty.notshowingupEmbed.fields[1][0], value: crafty.notshowingupEmbed.fields[1][1] }
   )
   .setTimestamp()
-  .setFooter({ text: config.notshowingupEmbed.footer.text, iconURL: config.notshowingupEmbed.footer.iconURL });
+  .setFooter({ text: crafty.notshowingupEmbed.footer.text, iconURL: crafty.notshowingupEmbed.footer.iconURL });
 
 const creditsecretEmbed = new EmbedBuilder()
-  .setColor(config.creditsecretEmbed.color)
-  .setTitle(config.creditsecretEmbed.title)
-  .setDescription(config.creditsecretEmbed.description)
-  .setThumbnail(config.creditsecretEmbed.thumbnail)
+  .setColor(crafty.creditsecretEmbed.color)
+  .setTitle(crafty.creditsecretEmbed.title)
+  .setDescription(crafty.creditsecretEmbed.description)
+  .setThumbnail(crafty.creditsecretEmbed.thumbnail)
   .addFields(
-    { name: config.creditsecretEmbed.fields[0][0], value: config.creditsecretEmbed.fields[0][1] }
+    { name: crafty.creditsecretEmbed.fields[0][0], value: crafty.creditsecretEmbed.fields[0][1] }
   )
   .setTimestamp()
-  .setFooter({ text: config.creditsecretEmbed.footer.text, iconURL: config.creditsecretEmbed.footer.iconURL });
+  .setFooter({ text: crafty.creditsecretEmbed.footer.text, iconURL: crafty.creditsecretEmbed.footer.iconURL });
 
 const creditsEmbed = new EmbedBuilder()
-  .setColor(config.creditsEmbed.color)
-  .setTitle(config.creditsEmbed.title)
-  .setDescription(config.creditsEmbed.description)
-  .setThumbnail(config.creditsEmbed.thumbnail)
+  .setColor(crafty.creditsEmbed.color)
+  .setTitle(crafty.creditsEmbed.title)
+  .setDescription(crafty.creditsEmbed.description)
+  .setThumbnail(crafty.creditsEmbed.thumbnail)
   .addFields(
-    { name: config.creditsEmbed.fields[0][0], value: config.creditsEmbed.fields[0][1] }
+    { name: crafty.creditsEmbed.fields[0][0], value: crafty.creditsEmbed.fields[0][1] }
   )
   .setTimestamp()
-  .setFooter({ text: config.creditsEmbed.footer.text, iconURL: config.creditsEmbed.footer.iconURL });
+  .setFooter({ text: crafty.creditsEmbed.footer.text, iconURL: crafty.creditsEmbed.footer.iconURL });
 
-client.on(ClientEvents.ready, () => {
-  console.log(`Crafty Online\nReady to help!`);
-  client.user.setActivity("!!help QC Support", { type: "LISTENING" })
-})
-
-client.on(ClientEvents.messageCreate, (message) => {
+client.on('messageCreate', function (message) {
   if (message.author.bot) return;
   let msg = message.content.toLowerCase()
   if (msg.startsWith(prefix)) {
