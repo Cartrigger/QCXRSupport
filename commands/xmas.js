9c56e7075139e1a26e45891de849c0d46eb41e83
The Judge <53906078+thejudge156@users.noreply.github.com>
2022-12-21 22:20:20 -0800

    xmas time 3

--- commands/xmas.js
+++ commands/xmas.js
@@ -10,6 +10,6 @@ module.exports = {
         let seconds = (currDate - date)/1000 % 60;
         let minutes = (currDate - date)/(1000*60) % 60;
         let hours = (currDate - date)/(1000*60*60) % 24;
-        await interaction.reply('There is ${seconds} seconds, ${minutes} minutes, and ${hours} hours till Christmas!');
+        await interaction.reply('There is ' + seconds + ' seconds, ' + minutes + ' minutes, and ' + hours + ' hours till Christmas!');
     },
 };
