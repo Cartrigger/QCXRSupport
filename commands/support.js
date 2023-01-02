c337b532555c55e81548250bddbdc2dc814b6841
Cart <cartrigger@gmail.com>
2023-01-02 02:24:08 -0500

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -5,6 +5,6 @@ module.exports = {
         .setName('support')
         .setDescription('Says the channel for support'),
     async execute(interaction) {
-        await interaction.reply('Sorry, but this isnt support, support is handeled in #945502948111290498 and you can see the FAQ in #821469293156892724');
+        await interaction.reply('Sorry, but this isnt support, support is handeled in #945502948111290498 and you can see the FAQ in #821469293156892724 . On top of this, you can see 3.0.1 common issues at #1056651999992623105');
     },
 };
