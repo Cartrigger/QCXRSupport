1a234135c1859b9abad107ee559cab2234ea055b
Cart <cartrigger@gmail.com>
2023-01-02 01:54:17 -0500

    out of spite

--- commands/github.js
+++ commands/github.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('github')
-        .setDescription('Tells you the github'),
+        .setDescription('Tells you the github for this bot.'),
     async execute(interaction) {
         await interaction.reply('https://github.com/Cartrigger/QCXRSoonBot');
     },
---
+++ commands/nopiracy.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('nopiracy')
+        .setDescription('Tells you information about piracy.'),
+    async execute(interaction) {
+        await interaction.reply('QCXR does not endorce piracy in any way shape or form. On top of this, we will not give Minecraft accounts to people who do not have such account.');
+    },
+};
