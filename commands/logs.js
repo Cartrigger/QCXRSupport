32f751ba32b30fd964740f6bfbf5e2c4a899a908
Cart <cartrigger@gmail.com>
2023-01-02 19:59:21 -0500

    k

--- commands/logs.js
+++ commands/logs.js
@@ -5,6 +5,6 @@ module.exports = {
         .setName('logs')
         .setDescription('Tells you where to find the logs for support.'),
     async execute(interaction) {
-        await interaction.reply('You can find logs for support in **(Quest 2) Android/data/com.qcxr.qcxr/.minecraft/latestlog.txt**, you can use any file manager to get to that location, then send it to the support member who asked for logs.');
+        await interaction.reply('You can find logs for support in **(Quest 2) Android/data/com.qcxr.qcxr/.minecraft/latestlog.txt**, you can use any file manager to get to that location, then send it to the support member who asked for logs. There is no information that someone could use to hack your account.');
     },
 };
--- commands/test.js
+++ commands/test.js
@@ -2,8 +2,8 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('aweroiuhterwiturehtreiuhotreiohu')
-        .setDescription('something'),
+        .setName('test')
+        .setDescription('test'),
     async execute(interaction) {
         await interaction.reply('test');
     },
