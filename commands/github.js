df4b28cf71dc8a618c22f94f78bd32a8983b16d9
TecEash1 <92249532+TecEash1@users.noreply.github.com>
2023-01-17 21:13:41 +0000

    Add commands back
    Added commands back with green embeds

---
+++ commands/github.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('github')
+        .setDescription('Tells you the github for this bot.'),
+    async execute(interaction) {
+        await interaction.reply('https://github.com/Cartrigger/QCXRSoonBot is my Github!');
+    },
+};
