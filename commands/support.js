55700cf4b95f21fcfbf93c19f48dfabdadff4b32
TechyGiraffe999 <92249532+TecEash1@users.noreply.github.com>
2023-01-17 22:41:22 +0000

    Update common issues channel
    - updated channel id - old one was removed

--- commands/support.js
+++ commands/support.js
@@ -7,7 +7,7 @@ module.exports = {
     async execute(interaction,client ) {
         const embed_support  = new EmbedBuilder()
         .setTitle("This isn't the support channel.")
-        .setDescription("Support is handled in <#945502948111290498> and you can see the FAQ in <#821469293156892724> . On top of this, you can see common issues in <#1056651999992623105>")
+        .setDescription("Support is handled in <#945502948111290498> and you can see the FAQ in <#821469293156892724> . On top of this, you can see common issues in <#1065000467010179092>")
         .setColor("Green")
         await interaction.reply({
             embeds: [embed_support]
