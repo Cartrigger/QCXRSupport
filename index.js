083a427dd090d762a95e9cc2c21925eb7dd0e6a2
Cart <cartrigger@gmail.com>
2023-02-01 04:03:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 const fs = require('fs');
 const path = require('node:path');
-const { Client, Collection, GatewayIntentBits, Routes } = require('discord.js');
+const { ask } = require("./ai.js");
+const { EmbedBuilder } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord.js');
 const { config } = require('dotenv');
+const spawn = require('child_process').spawn;
+const prefix = ('!')
 config();
-const Token = process.env.token;
-const ClientID = process.env.clientID;
-const GuildID = process.env.guildID;
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const token = process.env.token;
+const clientId = process.env.clientID;
+const guildId = process.env.guildID;
+
 const commands = [];
+
+const client = new Client({
+  intents:
+    [GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent]
+});
 
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+client.on(Events.MessageCreate, async message => {
+  if (message.mentions.has(client.user)) {
+    const prompt = message.content; //remove the exclamation mark from the message
+    console.log(prompt)
+    const answer = await ask(prompt); //prompt GPT-3
+    console.log(answer),
+      client.channels.fetch(message.channelId).then(channel => channel.send(answer), message.channel.sendTyping()),
+      message.channel.sendTyping();
+}});
+
 
 for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
@@ -26,15 +49,22 @@ for (const file of commandFiles) {
     commands.push(command.data.toJSON());
 }
 
-rest = new REST({version: '10'}).setToken(Token);
+const rest = new REST({ version: '10' }).setToken(token);
+
+client.on("ready", function () {
+    console.log(`the client becomes ready to start`);
+    console.log(`I am ready! Logged in as ${client.user.tag}!`);
+
+    client.user.setActivity("Online!");
+});
 
 client.on("rateLimit", function (rateLimitData) {
-    console.log(`the rate limit has been hit!`);
+    console.log(`the rate limit has been hit!  Slow'r down a tad.`);
     console.log({ rateLimitData });
 });
 
-rest.put(Routes.applicationGuildCommands(ClientID, GuildID), { body: commands })
-    .then(() => console.log('Successfully registered application commands. Welcome to the world of QuestCraft.'))
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+    .then(() => console.log('Successfully registered application commands. Welcome to the world of QCXR.'))
     .catch(console.error);
 
 client.on('interactionCreate', async interaction => {
@@ -49,15 +79,23 @@ client.on('interactionCreate', async interaction => {
     }
 });
 
-client.login(Token);
-
-client.on("ready", function () {
-    console.log(`the client becomes ready to start`);
-    console.log(`I am ready! Logged in as ${client.user.tag}!`);
-
-    client.user.setActivity("Online!");
-});
+client.login(token);
 
 client.once('ready', () => {
     console.log('Ready!');
 });
+
+require("readline").emitKeypressEvents(process.stdin);
+
+/*process.stdin.on("keypress", (char, evt) => {
+    if (char === "q") {
+        rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: [] })
+            .then(() => console.log('Successfully deleted all guild commands.'))
+            .catch(console.error);
+
+        rest.put(Routes.applicationCommands(clientId), { body: [] })
+            .then(() => console.log('Successfully deleted all application commands.'))
+            .catch(console.error);
+        setTimeout(()=> { process.exit(); }, 1000);
+    }
+});*/
