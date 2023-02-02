42c17e95b364b821bede5d2b2f1c75b9c1d50785
Cart <cartrigger@gmail.com>
2023-02-02 03:21:19 -0500

    a

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 const fs = require('fs');
 const path = require('node:path');
-const { Client, Collection, GatewayIntentBits, Routes, Events, ModalBuilder, ActionRowBuilder, TextInputBuilder, TextInputStyle, Interaction } = require('discord.js');
+const { EmbedBuilder, Events, StringSelectMenuBuilder, interaction } = require('discord.js');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord.js');
 const { config } = require('dotenv');
+const spawn = require('child_process').spawn;
 config();
-const Token = process.env.token;
-const ClientID = process.env.clientID;
-const GuildID = process.env.guildID;
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const token = process.env.token;
+const clientId = process.env.clientID;
+const guildId = process.env.guildID;
+
 const commands = [];
-const { SlashCommandBuilder, StringSelectMenuBuilder } = require('discord.js');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isStringSelectMenu()) return;
+
+    const selected = interaction.values.join(', ');
+
+    await interaction.update(`The user selected ${selected}!`);
+});
+
+client.on('interactionCreate', interaction => {
+
+    if (!interaction.isButton()) return;
+
+    if (interaction.customId == "id1") {
+
+    }
+
+});
 
 for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
@@ -26,14 +49,21 @@ for (const file of commandFiles) {
     commands.push(command.data.toJSON());
 }
 
-rest = new REST({version: '10'}).setToken(Token);
+const rest = new REST({ version: '10' }).setToken(token);
+
+client.on("ready", function () {
+    console.log(`the client becomes ready to start`);
+    console.log(`I am ready! Logged in as ${client.user.tag}!`);
+
+    client.user.setActivity("Online!");
+});
 
 client.on("rateLimit", function (rateLimitData) {
-    console.log(`the rate limit has been hit!`);
+    console.log(`the rate limit has been hit!  Slow'r down a tad.`);
     console.log({ rateLimitData });
 });
 
-rest.put(Routes.applicationGuildCommands(ClientID, GuildID), { body: commands })
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
     .then(() => console.log('Successfully registered application commands. Welcome to the world of QuestCraft.'))
     .catch(console.error);
 
@@ -45,19 +75,17 @@ client.on('interactionCreate', async interaction => {
         await command.execute(interaction);
     } catch (error) {
         console.error(error);
-        await interaction.reply({ content: 'There was an error while executing this command! Please contact <@317814254336081930>, <@719815864135712799> or <@654048865137459261>.', ephemeral: true });
+        await interaction.reply({ content: 'Oof! (COMMAND ERROR) Please contact <@317814254336081930>!', ephemeral: true });
     }
 });
 
 client.login(Token);
-
 client.on("ready", function () {
     console.log(`the client becomes ready to start`);
     console.log(`I am ready! Logged in as ${client.user.tag}!`);
 
     client.user.setActivity("Online!");
 });
-
 client.once('ready', () => {
     console.log('Ready!');
 });
