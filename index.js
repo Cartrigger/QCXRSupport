cbc0d36fe52e9d285287ce74a7f4b9d2ee805644
Cart <cartrigger@gmail.com>
2023-02-04 22:27:53 -0500

    ay

--- index.js
+++ index.js
@@ -132,6 +132,11 @@ const embed_mods  = new EmbedBuilder()
         .setDescription("**Adding mods to Questcraft 3.0:**\n*1. Install either the 1.18.2 or 1.19.3 fabric version of your mod to either your PC or your Quest\n2. If you are on PC, connect your Quest to Sidequest, and locate your mods folder at* ``android/data/com.qcxr.qcxr/files/.minecraft/mods.`` *If you are on your Quest, open a file manager like CX File Explorer, and locate the mods folder listed above\n3. Move your mod file into that version's folder.*\n\n**Note: You MUST have launched either 1.18.2 or 1.19.3 QuestCraft PRIOR to putting your mod into the mods folder.\nNote II: Make sure \"Hidden files\" is enabled in your file explorer** \n\n**Where to find mods:** \n **Modrinth: <https://modrinth.com/mods?f=categories%3A%27worldgen%27&g=categories%3A%27fabric%27&v=1.18.2,1.19.3>**\n **Curseforge: <https://www.curseforge.com/minecraft/mc-mods?filter-game-version=2020709689%3A7499&filter-sort=4>**\nwhen picking mods make sure to **only download 1.18.2/1.19.3 versions** depending on what version of questcraft you are playing ")
         .setColor("Green")
         .setFooter({ text: 'How to add mods written by Shradinx | Where to find mods written by TechyGiraffe999'})
+const embed_test = new EmbedBuilder()
+        .setTitle("test")
+        .setDescription("this is a test")
+        .setColor("Green")
+        .setFooter({ text: 'Test added by Cart'})
 client.on(Events.InteractionCreate, async interaction => {
 
     if(!interaction.isStringSelectMenu()) return;
@@ -185,6 +190,9 @@ client.on(Events.InteractionCreate, async interaction => {
     } 
     if (selected === "mods") {
         await interaction.reply({ embeds: [embed_mods], ephemeral: true })
-    } 
+    }
+    if (selected === "test") {
+        await interaction.reply({ embeds: [embed_test], ephemeral: true})
+    }
 })
 
