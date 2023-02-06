109595897ddf0212b937e688919a350a1a65a30f
Cart <cartrigger@gmail.com>
2023-02-05 21:35:44 -0500

    no token log lol and older djs

--- index.js
+++ index.js
@@ -21,8 +21,6 @@ for (const file of commandFiles) {
     client.commands.set(command.data.name, command);
 }
 
-console.log(Token)
-
 for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
