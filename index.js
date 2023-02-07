d2443b24fffeec4b600fe359d95379592ffce7ed
Cart <cartrigger@gmail.com>
2023-02-07 04:31:01 -0500

    test your code dimnut

--- index.js
+++ index.js
@@ -32,10 +32,10 @@ client.on("rateLimit", function (rateLimitData) {
     console.log({ rateLimitData });
 });
 
-rest.put(Routes.applicationGuildCommands(ClientID, GuildID), { body: commands })
+/*rest.put(Routes.applicationGuildCommands(ClientID, GuildID), { body: commands })
     .then(() => console.log('Successfully registered application commands locally with no errors! Your slash commands are working!'))
-    .catch(console.error);
-Routes.applicationCommands(ClientID), { body: commands }
+    .catch(console.error);*/
+rest.put(Routes.applicationCommands(ClientID), { body: commands })
     .then(() => console.log('Successfully registered application commands globally with no errors! Your slash commands are working!'))
     .catch(console.error);
 
