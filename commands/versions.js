d5d68bed627f361ba6f6ac439d9637fb238e4131
LimeDev8756 <100384210+LimeDev8756@users.noreply.github.com>
2023-02-26 17:00:54 +0000

    Update versions.js

--- commands/versions.js
+++ commands/versions.js
@@ -7,7 +7,7 @@ module.exports = {
     async execute(interaction,client ) {
         const embed_versions  = new EmbedBuilder()
         .setTitle("**Version info:**")
-        .setDescription("You are unable to use versions on questcraft other than 1.19.3 and 1.18.2 at the moment - however if your friend or you own a server you can install via version or via backwards so you can join on either questcraft version\n\nPaper/spigot (Plugins):\n<https://www.spigotmc.org/resources/viaversion.19254/>\n<https://www.spigotmc.org/resources/viabackwards.27448/>\n\nFabric:\n<https://www.curseforge.com/minecraft/mc-mods/viafabric>\n(Install info + other mods included on curseforge page)\n\nForge:\n<https://www.curseforge.com/minecraft/mc-mods/viaforge>\n(Install info + other mods included on curseforge page)")
+        .setDescription("You are unable to use versions on questcraft other than 1.19.3 and 1.18.2 at the moment - however if your friend or you own a Spigot/Paper server you can install [ViaVersion](https://www.spigotmc.org/resources/viaversion.19254/) or [ViaBackwards](https://www.spigotmc.org/resources/viabackwards.27448/) so you can join on either questcraft version. If you want to play on different versions on a Fabric server then you can see [the fabric version of this plugin](https://www.curseforge.com/minecraft/mc-mods/viafabric) or if you prefer Forge you can get the Forge version of that Fabric mod [here](https://www.curseforge.com/minecraft/mc-mods/viaforge)\nAll installation instructions are located on the SpigotMC/CurseForge page")
         .setColor("Green")
         .setFooter({ text: 'Written by TechyGiraffe999'})
         await interaction.reply({
