d2bd0dbe0ba7676ede14d3c9458e257af51d654e
LimeDev8756 <100384210+LimeDev8756@users.noreply.github.com>
2023-02-26 16:50:18 +0000

    Update qcgithub.js

--- commands/qcgithub.js
+++ commands/qcgithub.js
@@ -5,6 +5,6 @@ module.exports = {
         .setName('qcgithub')
         .setDescription('Tells you where to find the QuestCraft Github.'),
     async execute(interaction) {
-        await interaction.reply('The QuestCraft Github is https://github.com/questcraftplusplus any other Github is potentially dangerous and any APK should not be downloaded from it.');
+        await interaction.reply('The QuestCraft Github is located [here](https://github.com/questcraftplusplus) any other Github is potentially dangerous and any APK should not be downloaded from it. Here be dragons!');
     },
 };
