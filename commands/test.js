12a7b5ed8e526c3396b6f4f7fbafe43c9dfe37a1
LimeDev8756 <100384210+LimeDev8756@users.noreply.github.com>
2023-02-26 16:55:54 +0000

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder, EmbedBuilder, Embed } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('test')
-        .setDescription('test'),
+        .setDescription('This is a test because if something breaks we need to test and what better why to do it then with a test which sends an embed containing the word test twice, by the way this is a test.'),
     async execute(interaction,client ) {
         const embed_test = new EmbedBuilder()
             .setTitle("This is a test")
