fbf5aa0be76f01ec28a3023640ea0a7b669d2e03
LimeDev8756 <100384210+LimeDev8756@users.noreply.github.com>
2023-02-26 16:56:29 +0000

    Update unity.js

--- commands/unity.js
+++ commands/unity.js
@@ -7,7 +7,7 @@ module.exports = {
     async execute(interaction,client ) {
         const embed_unity  = new EmbedBuilder()
         .setTitle("The new 3.0+ Launcher")
-        .setDescription("The 3.0+ launcher is made in Unity, with Pojlib, which makes it where you can launch into VR mode and play the game again.")
+        .setDescription("The 3.0+ launcher is made in Unity, with Pojlib, which makes it where you can launch into VR which means there is no need for a workaround.")
         .setColor("Green")
         await interaction.reply({
             embeds: [embed_unity]
