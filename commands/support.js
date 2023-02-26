ee763dac81ffb66133502aff57d9ed1401c54f2c
LimeDev8756 <100384210+LimeDev8756@users.noreply.github.com>
2023-02-26 16:54:23 +0000

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -6,7 +6,7 @@ module.exports = {
         .setDescription('Says the channel for support'),
     async execute(interaction,client ) {
         const embed_support  = new EmbedBuilder()
-        .setTitle("This isn't the support channel.")
+        .setTitle("Sorry but <:notsupportchannel:993043214472859778>")
         .setDescription("Support is handled in <#945502948111290498> and you can see the FAQ in <#821469293156892724> . On top of this, you can see common issues in <#1065000467010179092>")
         .setColor("Green")
         await interaction.reply({
