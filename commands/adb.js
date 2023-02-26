e37eba9ad547b83fa70b0c329ddca5b67b5aaeb2
LimeDev8756 <100384210+LimeDev8756@users.noreply.github.com>
2023-02-26 16:36:33 +0000

    Update adb.js

--- commands/adb.js
+++ commands/adb.js
@@ -7,7 +7,8 @@ module.exports = {
     async execute(interaction,client ) {
         const embed_adb  = new EmbedBuilder()
         .setTitle("uninstalling using ADB commands.")
-        .setDescription("To uninstall completely if you cannot install again, the command is: ```adb uninstall com.qcxr.qcxr```")
+        .setDescription("To uninstall completely if you cannot install again, the command is: ```adb uninstall com.qcxr.qcxr```\n\nTo run ADB commands using SideQuest (advanced only) you can see the image below")
+        .setImage("https://cdn.discordapp.com/attachments/957344047494725672/1079441415458783282/image.png")
         .setColor("Green")
         await interaction.reply({
             embeds: [embed_adb]
