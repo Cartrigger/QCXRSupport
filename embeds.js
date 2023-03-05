93a13d51a3eded2b603bf9495d44fc57927361c9
TechyGiraffe999 <92249532+TecEash1@users.noreply.github.com>
2023-03-05 15:15:35 +0000

    trolly

--- embeds.js
+++ embeds.js
@@ -73,7 +73,7 @@ const embeds = {
     .setTitle("**Version info:**")
     .setDescription("You are unable to use versions on QuestCraft other than 1.19.3 and 1.18.2 at the moment - however if your friend or you own a Spigot/Paper server you can install [ViaVersion](https://www.spigotmc.org/resources/viaversion.19254/) or [ViaBackwards](https://www.spigotmc.org/resources/viabackwards.27448/) so you can join on either questcraft version. If you want to play on different versions on a Fabric server then you can see [the fabric version of this plugin](https://www.curseforge.com/minecraft/mc-mods/viafabric) or if you prefer Forge you can get the Forge version of that Fabric mod [here](https://www.curseforge.com/minecraft/mc-mods/viaforge)\nAll installation instructions are located on the SpigotMC/CurseForge page")
     .setColor("Green")
-    .setFooter({ text: 'Written by TechyGiraffe999, Corrected by DevLime'}),
+    .setFooter({ text: 'Written by TechyGiraffe999, Updated by DevLime'}),
   yvr: new EmbedBuilder()
     .setTitle("YVR info:")
     .setDescription("YVR is a Chinese headset company, they have requested we port QuestCraft. We are working on both QCXR and YVRCraft at the same time.")
