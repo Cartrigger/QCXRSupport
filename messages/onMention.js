f71eb3452242842b1e5f65dfde573dde7db3979c
TechyGiraffe999 <92249532+TecEash1@users.noreply.github.com>
2023-03-09 19:58:45 +0000

    mention is now an embed

--- messages/onMention.js
+++ messages/onMention.js
@@ -3,19 +3,23 @@
  * @author Naman Vrati
  * @since 3.0.0
  */
-
+const { EmbedBuilder } = require("discord.js");
 const { prefix } = require("../config.json");
+
 
 module.exports = {
 	/**
 	 * @description Executes when the bot is pinged.
 	 * @author Naman Vrati
-	 * @param {import('discord.js').Message} message The Message Object of the command.
+	 * @param {import('discord.js').Message} message} The Message Object of the command.
 	 */
 
+
+
 	async execute(message) {
-		return message.channel.send(
-			`Hi ${message.author}! I am Crafty! You can get help / see my full list of commands by running /help`
-		);
+		const mention  = new EmbedBuilder()
+			.setDescription(`Hi ${message.author}! I am Crafty! To see my full list of commands, run \`/help\``)
+			.setColor("Gold")
+		return message.reply({ embeds: [mention]});
 	},
 };
