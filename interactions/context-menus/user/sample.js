e36ee8604acbb2fda811b927ab3eb4ed00078a29
TechyGiraffe999 <92249532+TecEash1@users.noreply.github.com>
2023-03-09 19:09:15 +0000

    fix context menus

--- interactions/context-menus/user/sample.js
+++ interactions/context-menus/user/sample.js
@@ -1,23 +1,23 @@
-/**
- * @file Sample Use Context Menu interaction
- * @author Krish Garg
- * @since 3.0.0
- * @version 3.2.2
- */
+// /**
+//  * @file Sample Use Context Menu interaction
+//  * @author Krish Garg
+//  * @since 3.0.0
+//  * @version 3.2.2
+//  */
 
-/**
- * @type {import('../../../typings').ContextInteractionCommand}
- */
-module.exports = {
-	data: {
-		name: "sample",
-		type: 2, // 2 is for user context menus
-	},
+// /**
+//  * @type {import('../../../typings').ContextInteractionCommand}
+//  */
+// module.exports = {
+// 	data: {
+// 		name: "sample",
+// 		type: 2, // 2 is for user context menus   <--- Comment out to use context menus (remove all // at the start of lines)
+// 	},
 
-	async execute(interaction) {
-		await interaction.reply({
-			content: "I am a sample user context menu.",
-		});
-		return;
-	},
-};
+// 	async execute(interaction) {
+// 		await interaction.reply({
+// 			content: "I am a sample user context menu.",
+// 		});
+// 		return;
+// 	},
+// };
