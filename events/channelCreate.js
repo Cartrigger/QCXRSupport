47c4a3d10b7b427cc117696571cd380f997a1bb5
TechyGiraffe999 <92249532+TecEash1@users.noreply.github.com>
2023-03-28 19:28:36 +0100

    crafty solves tickets login

---
+++ events/channelCreate.js
@@ -0,0 +1,17 @@
+const { EmbedBuilder } = require("discord.js");
+
+
+module.exports = {
+    name: 'channelCreate',
+    async execute(channel) {
+        console.log(channel.name);
+        channel_temp = channel.parent
+        console.log(channel_temp)
+        
+        if (channel_temp.id === '1089978659869294612' && channel.name.slice(0, 5) === 'login') {
+            const message = await channel.send("Testing for crafty");
+            message.edit("New login channel created: " + channel.name);
+            messae
+        }
+    },
+};
