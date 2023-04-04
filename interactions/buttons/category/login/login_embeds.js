50ede5352d1438f8c931cfd646809593c7a0cff7
alex feliow <98061642+DED0026@users.noreply.github.com>
2023-04-04 18:28:38 -0500

    Update login_embeds.js

--- interactions/buttons/category/login/login_embeds.js
+++ interactions/buttons/category/login/login_embeds.js
@@ -12,7 +12,7 @@ const login_embeds = {
     .setDescription("You can check via this [link](https://www.minecraft.net/en-us/msaprofile)\nMake Sure Minecraft: Java edition looks identical to the one in the image")
     .setImage("https://media.discordapp.net/attachments/1057074981135196230/1092925442610188369/image.png?width=1347&height=670")
     .setColor("Green")
-    .setFooter({ text: 'Written by !DED [BETA]'}),
+    .setFooter({ text: 'Written by !DED [BETA]', iconURL: 'https://cdn.discordapp.com/avatars/483359783831732255/be45184a1568fa89c5ea033b9c12948c.png?size=4096'}),
   own_no: new EmbedBuilder()
     .setTitle("You must own Java Edition to play QuestCraft")
     .setDescription("You can purchase it via this [link](https://www.minecraft.net/en-us/store/minecraft-java-bedrock-edition-pc)\nOnce you have done that please make a username via this [link](https://www.minecraft.net/en-us/msaprofile/mygames/editprofile) and try play again, once you have done that please try play again.\n\n*If your issue still persists please run ``/login``, and click the other button*\n\nTip to save money, Only buy the base game.")
@@ -46,7 +46,7 @@ const login_embeds = {
   
   others: new EmbedBuilder()
     .setTitle("If none of the things mentioned have worked")
-    .setDescription("Here are are the direct troubleshooting steps to resolve this. \nYou must have minecraft java.\nYou must have a username set up for minecraft java.\n your account must be overage and if it isnt it must be added to a family.\nTry reinstalling questcraft\nTry running minecraft java on a pc once.")
+    .setDescription("Here are are the direct troubleshooting steps to resolve this. \nYou must have minecraft java.\nYou must have a username set up for minecraft java.\n your account must be overage and if it isnt it must be added to a family.\nTry reinstalling questcraft\nTry running minecraft java on a pc once.\nIf your issue still persists, please wait for a support staff member to review your issue, please provide any additional information or create a ticket"")
     .setColor("Green")
     .setFooter({ text: 'Written by !DED/TechyGiraffe999 [BETA]', iconURL: 'https://images-ext-1.discordapp.net/external/uVW63D2qclMVEwdF5yp21Fqi7sOmOzNWyRiRiWhTGAM/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/719815864135712799/08a1cc347410b8444f0ef68dc39c17a5.png?width=303&height=303'}),  
 };
