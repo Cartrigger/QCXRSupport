ddde14657e6d737b6eebd2b30d03c7f0447f4b73
TechyGiraffe999 <92249532+TecEash1@users.noreply.github.com>
2023-04-04 11:23:52 +0100

    limit access to command in support channels

--- interactions/slash/misc/supportchannels.js
+++ interactions/slash/misc/supportchannels.js
@@ -1,17 +1,26 @@
-// Deconstructed the constants we need in this file.
-
 const { EmbedBuilder, SlashCommandBuilder } = require("discord.js");
-
-/**
- * @type {import('../../../typings').SlashInteractionCommand}
- */
 const embeds = require('../../../embeds.js');
+
+const error = new EmbedBuilder()
+    .setTitle('Error!')
+    .setDescription('This command cannot be used in this channel ')
+    .setColor('Red')
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('supportchannels')
         .setDescription('Mentions the channels for support'),
-    async execute(interaction,client ) {
+    async execute(interaction, client) {
+        const channelExceptions = ['945502948111290498', '946021441298649158']; 
+        
+        if (channelExceptions.includes(interaction.channelId)) {
+            await interaction.reply({
+                embeds: [error],
+                ephemeral: true 
+            });
+            return;
+        }
+
         await interaction.reply({
             embeds: [embeds.support_channels]
         });
