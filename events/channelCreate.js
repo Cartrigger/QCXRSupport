60efe9ea56baf42b5fd21b95d157abfe98543e1d
TechyGiraffe999 <92249532+TecEash1@users.noreply.github.com>
2023-04-04 10:16:51 +0100

    more error handling

--- events/channelCreate.js
+++ events/channelCreate.js
@@ -6,18 +6,18 @@ const buttons = require('../interactions/buttons/category/login/v51/v51_buttons'
 module.exports = {
     name: 'channelCreate',
     async execute(channel) {
+        try{
         channel_temp = channel.parent
-        if (channel_temp.id === '946184804657934469' && channel.name.slice(0, 5) === 'login') {
+        if (channel_temp.id === '1089978659869294612' && channel.name.slice(0, 5) === 'login') {
             //const message = await channel.send("Testing for crafty");
             //message.edit("New login channel created: " + channel.name);
             
-            try{
+            
                 await new Promise(resolve => setTimeout(resolve, 1000)); // 1 second delay
                 const start_message = await channel.send({embeds: [login_embeds.login]});
             
                 const message = await channel.send({embeds: [login_embeds.v51], components: [buttons.v51] });
-            } catch(err) {
+            }} catch(err) {
         }
         }
-    },
-};
+    }
