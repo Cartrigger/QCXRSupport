77fd3b07bba6576ecb1333edaed1c083a1e5c04e
Jaffie Waffie <94018839+JaffaJace@users.noreply.github.com>
2023-04-05 14:31:54 +0100

    instances buttons perhap?

---
+++ interactions/buttons/category/ingame/instances/instances_yes.js
@@ -0,0 +1,16 @@
+
+/**
+ * @type {import('../../../typings').ButtonInteractionCommand}
+ */
+
+const ingame_embeds = require('../ingame_embeds');
+//const buttons = require('../mods/mods_buttons');
+
+module.exports = {
+	id: "instances_yes",
+
+	async execute(interaction, message) {
+		await interaction.update({embeds: [ingame_embeds.mods]/*, components: [buttons.account_status]*/});
+		return;
+	},
+};
--- interactions/slash/misc/ingame.js
+++ interactions/slash/misc/ingame.js
@@ -7,7 +7,7 @@ const { EmbedBuilder, SlashCommandBuilder } = require("discord.js");
  */
 const embeds = require('../../../embeds.js');
 const ingame_embeds = require('../../buttons/category/ingame/ingame_embeds');
-//const buttons = require('../../buttons/category/ingame/v51/v51_buttons')
+const buttons = require('../../buttons/category/ingame/instances/instances_buttons')
 
 
 module.exports = {
@@ -15,6 +15,6 @@ module.exports = {
         .setName('ingame')
         .setDescription('Info on how to solve ingame issues'),
     async execute(interaction,client ) {
-        await interaction.reply({embeds: [ingame_embeds.v51],ephemeral: true });
+        await interaction.reply({embeds: [ingame_embeds.instances],components: [buttons.instances],ephemeral: true });
     },
 };
