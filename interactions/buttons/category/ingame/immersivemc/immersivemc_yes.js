250b7e112c002304e0016508ffe235336d957b2a
Jaffie Waffie <94018839+JaffaJace@users.noreply.github.com>
2023-04-05 15:45:02 +0100

    as they say, mistakes is the way to learn

--- interactions/buttons/category/ingame/immersivemc/immersivemc_yes.js
+++ interactions/buttons/category/ingame/immersivemc/immersivemc_yes.js
@@ -7,7 +7,7 @@ const embeds = require('../ingame_embeds');
 const buttons = require('../immersivemc/immersivemc_buttons');
 
 module.exports = {
-	id: "instances_yes",
+	id: "immersivemc_yes",
 
 	async execute(interaction, message) {
 		await interaction.update({embeds: [ingame_embeds.immersivemc_yes], components: []});
