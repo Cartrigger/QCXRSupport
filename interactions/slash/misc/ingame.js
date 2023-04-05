6692c35824fe69c3b5fbf07f9f40056a0dbc90ce
Jaffie Waffie <94018839+JaffaJace@users.noreply.github.com>
2023-04-05 01:40:29 +0100

    created slash command

---
+++ interactions/slash/misc/ingame.js
@@ -0,0 +1,20 @@
+// Deconstructed the constants we need in this file.
+
+const { EmbedBuilder, SlashCommandBuilder } = require("discord.js");
+
+/**
+ * @type {import('../../../typings').SlashInteractionCommand}
+ */
+const embeds = require('../../../embeds.js');
+const ingame_embeds = require('../../buttons/category/ingame/ingame_embeds');
+//const buttons = require('../../buttons/category/ingame/v51/v51_buttons')
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ingame')
+        .setDescription('Info on how to solve ingame issues'),
+    async execute(interaction,client ) {
+        await interaction.reply({embeds: [ingame_embeds.v51],ephemeral: true });
+    },
+};
