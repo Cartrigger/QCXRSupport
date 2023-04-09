5dd4804aa5358846be1263123691a35b319d6404
Jaffie Waffie <94018839+JaffaJace@users.noreply.github.com>
2023-04-09 12:09:20 +0100

    instances removed, people wouldnt understand anyway

--- interactions/buttons/category/ingame/ingame_embeds.js
+++ interactions/buttons/category/ingame/ingame_embeds.js
@@ -17,18 +17,7 @@ const ingame_embeds = {
     .setDescription("A support member will be with you shorty to review your screenshot, if you have not sent it yet please send it below\n\n*If you are not in a ticket please make one and send the screenshot there if a support member is currently unavailable* ")
     .setColor("Green")
     .setFooter({ text: 'Written by TechyGiraffe999 [BETA]', iconURL: 'https://images-ext-1.discordapp.net/external/uVW63D2qclMVEwdF5yp21Fqi7sOmOzNWyRiRiWhTGAM/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/719815864135712799/08a1cc347410b8444f0ef68dc39c17a5.png?width=303&height=303'}),
-
-  instances: new EmbedBuilder()
-    .setTitle("Have your instances downloaded successfully?")
-    .setDescription("Sometimes the instances can fail to download for various reasons. If your instances only show one instance (you cant change between 1.18.2 and 1.19.3) or neither at all, that means your instances failed to download for whatever reason. If you can see both 1.18.2 and 1.19.3 then your instances downloaded successfully.")
-    .setColor("Blue")
-    .setFooter({ text: 'Developed by JaffaJace', iconURL: 'https://cdn.discordapp.com/avatars/654048865137459261/ec3ef49d087775eb2c26b10a4dc7c456.png?size=4096'}),
-  instances_no: new EmbedBuilder()
-    .setTitle("Instances download failure")
-    .setDescription("To fix the instance download issue is simple, you just have to reinstall QuestCraft fresh. Additionally, you can do `/download Reinstall` for precise steps on how to do this.")
-    .setColor("Blue")
-    .setFooter({ text: 'Developed by JaffaJace', iconURL: 'https://cdn.discordapp.com/avatars/654048865137459261/ec3ef49d087775eb2c26b10a4dc7c456.png?size=4096'}),
-
+    
   mods: new EmbedBuilder()
     .setTitle("Did you install mods?")
     .setDescription("Questcraft will often have issues with most complex and even some simple mods, and many people can and do have issues regarding incompatible mods.")
--- interactions/buttons/category/ingame/v51/v51_no.js
+++ interactions/buttons/category/ingame/v51/v51_no.js
@@ -4,13 +4,13 @@
  */
 
 const ingame_embeds = require('../ingame_embeds');
-const buttons = require('../instances/instances_buttons');
+const buttons = require('../mods/mods_buttons');
 
 module.exports = {
 	id: "v51_no",
 
 	async execute(interaction, message) {
-		await interaction.update({embeds: [ingame_embeds.instances], components: [buttons.instances]});
+		await interaction.update({embeds: [ingame_embeds.mods], components: [buttons.mods]});
 		return;
 	},
 };
