917ae53bd9a464e0c89d67be644d2bc0b45c1203
Jaffie Waffie <94018839+JaffaJace@users.noreply.github.com>
2023-04-16 22:28:06 +0100

    okay that should be it

--- interactions/buttons/category/ingame/ingame_embeds.js
+++ interactions/buttons/category/ingame/ingame_embeds.js
@@ -7,13 +7,6 @@ const ingame_embeds = {
     .setColor("Blue")
     .setFooter({ text: 'Developed by JaffaJace', iconURL: 'https://cdn.discordapp.com/avatars/654048865137459261/ec3ef49d087775eb2c26b10a4dc7c456.png?size=4096'}),
   
-// Moved V51 to the main embeds file
-  v51_yes: new EmbedBuilder()
-    .setTitle("Please bear with us")
-    .setDescription("A support member will be with you shortly to review your screenshot, if you have not sent it yet please send it below\n\n*If you are not in a ticket please make one and send the screenshot there if a support member is currently unavailable* ")
-    .setColor("Green")
-    .setFooter({ text: 'Written by TechyGiraffe999', iconURL: 'https://images-ext-1.discordapp.net/external/uVW63D2qclMVEwdF5yp21Fqi7sOmOzNWyRiRiWhTGAM/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/719815864135712799/08a1cc347410b8444f0ef68dc39c17a5.png?width=303&height=303'}),
-    
   mods: new EmbedBuilder()
     .setTitle("Did you install mods?")
     .setDescription("Questcraft will often have issues with most complex and even some simple mods, and many people can and do have issues regarding incompatible mods.")
--- interactions/slash/misc/ingame.js
+++ interactions/slash/misc/ingame.js
@@ -5,14 +5,14 @@ const { EmbedBuilder, SlashCommandBuilder } = require("discord.js");
 /**
  * @type {import('../../../typings').SlashInteractionCommand}
  */
-const embeds = require('../../../embeds.js');
-const buttons = require('../../buttons/category/ingame/v51/v51_buttons')
+const ingame_embeds = require('../../buttons/category/ingame/ingame_embeds');
+const buttons = require('../../buttons/category/ingame/mods/mods_buttons')
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ingame')
         .setDescription('Info on how to solve ingame issues'),
     async execute(interaction,client ) {
-        await interaction.reply({embeds: [embeds.v51],components: [buttons.v51],ephemeral: true });
+        await interaction.reply({embeds: [ingame_embeds.mods],components: [buttons.mods],ephemeral: true });
     },
 };
