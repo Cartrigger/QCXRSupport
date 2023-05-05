19f6f5f2b91d7ccce8817d121f50ec317da0ca4a
TechyGiraffe999 <92249532+TecEash1@users.noreply.github.com>
2023-05-05 01:03:19 +0100

    Fix known issues channel
    (In supportchannels embed)

--- embeds.js
+++ embeds.js
@@ -69,7 +69,7 @@ const embeds = {
   
     support_channels: new EmbedBuilder()
     .setTitle("**Sorry but <:notsupportchannel:993043214472859778>**")
-    .setDescription("Support is handled in <#945502948111290498> or <#946021441298649158> and you can see the FAQ in <#821469293156892724>.\nOn top of this, you can see common issues in <#1065000467010179092>\n\nFor more guided support, you can make a ticket in <#946184706486054984>, and a support member will be with you shortly.")
+    .setDescription("Support is handled in <#945502948111290498> or <#946021441298649158> and you can see the FAQ in <#821469293156892724>.\nOn top of this, you can see common issues in <#1101947003111489586>\n\nFor more guided support, you can make a ticket in <#946184706486054984>, and a support member will be with you shortly.")
     .setColor("Green"),
   
     versions: new EmbedBuilder()
