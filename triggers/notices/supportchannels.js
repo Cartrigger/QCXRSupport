3a54158defa1e57eb4ba43c35b5088a882e89d77
TechyGiraffe999 <92249532+TecEash1@users.noreply.github.com>
2023-05-30 20:49:19 +0100

    add support staff channel exception

--- triggers/notices/supportchannels.js
+++ triggers/notices/supportchannels.js
@@ -9,7 +9,7 @@ module.exports = {
 
 	execute(message, args) {
 		// Put all your trigger code over here. This code will be executed when any of the element in the "name" array is found in the message content.
-		const channelExceptions = ['945502948111290498', '946021441298649158',"946184804657934469"]; 
+		const channelExceptions = ['945502948111290498', '946021441298649158',"946184804657934469","1057074981135196230"]; 
 		if (channelExceptions.includes(message.channelId) || channelExceptions.includes(message.channel.parentId)) {
 			return;
 		}
