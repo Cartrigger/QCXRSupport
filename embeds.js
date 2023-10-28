34c4eb071482143c2c5a48961c0c45bd04a7e64c
Cart <cartrigger@gmail.com>
2023-10-28 01:38:07 -0400

    forgor image :skull:

--- embeds.js
+++ embeds.js
@@ -230,7 +230,7 @@ const embeds = {
       .setTitle("Known Compatible Servers")
       .setDescription("### Rosslight Network\n`mc.rosslight.network`\nRun by Rosslight and some of XRCraft's former team\n\n### Bones Network\n`play.bones.network`\nRun by !DED#0026 and bonesmgee\n\n### TSC\n`smp.playtsc.com`\nRun By ungerground2\n\n### Scrabble SMP\n`16.ip.gl.ply.gg:44321`\nRun by monopolystinks")
       .setColor("DarkBlue")
-      .setFooter({ text: 'Written by Cartri, contributed to by ungerground2', iconURL: ''}),
+      .setFooter({ text: 'Written by Cartri, contributed to by ungerground2', iconURL: 'https://cdn.discordapp.com/avatars/317814254336081930/a_89da6baf3d01206f8bd3e0f4ac91295f.gif?size=1024&width=0&height=256'}),
 
     essentials_questcraft: new EmbedBuilder()
     .setTitle("**Essentials Mod on QuestCraft**")
