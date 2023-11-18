a6663aaf1e3ace1eae3de6e91fac431217dbb804
TechyGiraffe999 <92249532+TecEash1@users.noreply.github.com>
2023-11-18 13:01:06 +0000

    Update embeds.js

--- embeds.js
+++ embeds.js
@@ -18,7 +18,7 @@ const embeds = {
 
 	developers: new EmbedBuilder()
 		.setTitle("**🛠️ My Developers!**")
-		.setDescription("The current developers of <@997670790604542012> are <@719815864135712799> and <@317814254336081930>! \nYou can checkout the full code of <@997670790604542012> at the [GitHub repo](https://github.com/Cartrigger/QCXRSupport)")
+		.setDescription("The current developers of <@997670790604542012> are <@719815864135712799> and <@317814254336081930>! \nYou can checkout the full code of <@997670790604542012> at the [**GitHub repo**](https://github.com/Cartrigger/QCXRSupport).")
 		.setColor("Green"),
 
 	installation: new EmbedBuilder()
