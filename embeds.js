1eadb1210c2e161f25abc40b75901bfdf4fb0eee
Cart <cartrigger@gmail.com>
2023-11-24 15:00:12 -0500

    This is going to be what part 3 is gonna be :crying:

--- embeds.js
+++ embeds.js
@@ -1,16 +1,6 @@
 const { EmbedBuilder } = require("discord.js");
 
 const embeds = {
-	adb: new EmbedBuilder()
-		.setTitle("**Uninstalling using ADB commands**")
-		.setDescription("If you're having an issue with uninstalling and need to perform a force uninstallation, Please make sure you are using **[SideQuest Advanced](https://SideQuestvr.com/setup-howto)** as basic SideQuest wont work. Run the command: ``adb uninstall com.qcxr.qcxr``, as shown in the image below.")
-		.setImage("https://media.discordapp.net/attachments/1059913125308145716/1111366186274390226/ADB.png?width=643&height=670")
-		.setColor("Green")
-		.setFooter({
-			text: "Written by my Developers",
-			iconURL: "https://images-ext-1.discordapp.net/external/5cPanAYF2wOpIhmzrCkina2UpawRtHHU4y2KjFviqCI/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/997670790604542012/92a28b5bb82c7512fe8711fa2fd63163.png?width=633&height=633"
-		}),
-
 	bedrock: new EmbedBuilder()
 		.setTitle("**QuestCraft Bedrock Edition**")
 		.setDescription("QuestCraft doesn't have any immediate plans to introduce Bedrock due to the Minecraft TOS.\n\nIt is also incredibly difficult to reverse eningeer games coded in C++, which bedrock is coded in.\nIf you want bedrock vr, it does exist for pcvr. You can find info [here](https://www.minecraft.net/en-us/vr).")
