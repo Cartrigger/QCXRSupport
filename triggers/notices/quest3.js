4bddad510829926f93d0b07455ccb41db6de4754
Cart <cartrigger@gmail.com>
2024-04-23 16:47:11 -0400

    make it less funny

--- triggers/notices/quest3.js
+++ triggers/notices/quest3.js
@@ -8,12 +8,12 @@ module.exports = {
 
 	execute(message, args) {
 		// Put all your trigger code over here. This code will be executed when any of the element in the "name" array is found in the message content.
-		const allowedChannels = ["945502948111290498, 946021441298649158"];
+		const dissallowedChannels = ["945502948111290498, 946021441298649158"];
 		const embed_Quest3 = new EmbedBuilder()
 			.setTitle("**Quest 3 is not out yet!**")
 			.setDescription("Quest 3 is not yet out for the public, but is available, to check out the Patreon, please do /patreon, if you are asking for a ETA, we dont have one yet, but it will be out as soon as it is finished.")
 			.setColor("Orange")
-		if (!allowedChannels.includes(message.channelId))
+		if (!dissallowedChannels.includes(message.channelId))
 		{
 			return;
 		}
