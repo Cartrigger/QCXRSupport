2e13f6311e7e9f23cebbf9d864bc6a1f10da3e4e
Cart <cartrigger@gmail.com>
2024-05-11 00:44:19 -0400

    pancakes

--- triggers/notices/supportchannels.js
+++ triggers/notices/supportchannels.js
@@ -1,9 +1,8 @@
 const {EmbedBuilder} = require("discord.js");
 module.exports = {
-	name: ["<:notsupportchannel:993043214472859778>"], //Put the trigger words in the array to use it
+	name: ["<:notsupportchannel:993043214472859778>"],
 
 	execute(message, args) {
-		// Put all your trigger code over here. This code will be executed when any of the element in the "name" array is found in the message content.
 		const channelExceptions = ["945502948111290498", "946021441298649158", "1057074981135196230", "821078174992957480", "821076673331724309", "932673625813823518"];
 		const embed_support_channels = new EmbedBuilder()
 			.setTitle("**Sorry but <:notsupportchannel:993043214472859778>**")
