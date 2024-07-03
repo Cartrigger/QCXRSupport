d2d74f63fcd57d6586135648a085ec280f53942e
Cart <cartrigger@gmail.com>
2024-07-03 00:27:48 -0400

    Add this for now until it works

--- triggers/notices/supportchannels.js
+++ triggers/notices/supportchannels.js
@@ -7,12 +7,12 @@ module.exports = {
     name: ["<:notsupportchannel:993043214472859778>"],
 
     execute(message, args) {
+        console.log("SupportChannel Triggered!")
         const channelInclude = ["820767484042018832, 820774522244038686, 820781731706699826, 947230579659800646, 955140205881462854, 1201007124524843068, 1060919089981308948, 852634389078081557, 945446101849374771, 1221910504499581058"];
         if (channelInclude.includes(message.channelId) || channelInclude.includes(message.channel.parentId)) {
             message.channel.send({
                 embeds: [embed_support_channels]
             });
         }
-
     }
 };
