aa86433e4795b750c7d8f7c244c598ce47746816
Cart <cartrigger@gmail.com>
2024-07-28 02:34:43 -0400

    Quick Fix!

--- interactions/slash/java.js
+++ interactions/slash/java.js
@@ -4,7 +4,7 @@ const embed_java = new EmbedBuilder()
     .setTitle("**About Minecraft Java**")
     .setDescription("First and foremost, QuestCraft uses Minecraft Java.\n\nJava Edition of Minecraft is a version of Minecraft originally made in 2009. It is the original verison of Minecraft. **QuestCraft uses the Java Edition** to give a VR experience with low complications.\n\nJava Edition is not Bedrock Edition, but if you buy Java, it comes with Bedrock out of the box for Windows PC's.")
     .setColor("Green")
-module.exports = {embed_bedrock}
+module.exports = {embed_java}
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("java")
