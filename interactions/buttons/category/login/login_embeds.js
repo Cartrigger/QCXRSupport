225f9ad134adf175bd04328e67eefa7d5c42e02e
Cart <cartrigger@gmail.com>
2024-08-14 13:44:04 -0400

    Bring back Own to tickets, make it all come from the embed file.

--- interactions/buttons/category/login/login_embeds.js
+++ interactions/buttons/category/login/login_embeds.js
@@ -10,6 +10,15 @@ const login_embeds = {
             iconURL: "https://cdn.discordapp.com/attachments/1059913125308145716/1157685309602013304/TechyGiraffe_v.2.0_test_3.png?ex=651981f6&is=65183076&hm=b630b14532ae9e04c63949a94b12d9308431e6d7726cead62bca42ba4720f28d&"
         }),
 
+    own: new EmbedBuilder()
+        .setTitle("Do you own Minecraft Java Edition?")
+        .setDescription("You can check via this [link](https://www.minecraft.net/en-us/msaprofile/mygames/editprofile)\n\n IF you do own it and havn't made a username there already, please make one")
+        .setColor("Green")
+        .setFooter({
+            text: "Written by TechyGiraffe999 [BETA]",
+            iconURL: "https://cdn.discordapp.com/attachments/1059913125308145716/1157685309602013304/TechyGiraffe_v.2.0_test_3.png?ex=651981f6&is=65183076&hm=b630b14532ae9e04c63949a94b12d9308431e6d7726cead62bca42ba4720f28d&"
+        }),
+
     own_no: new EmbedBuilder()
         .setTitle("You must own Java Edition to play QuestCraft")
         .setDescription("You can purchase it via this [link](https://www.minecraft.net/en-us/store/minecraft-java-bedrock-edition-pc)\nOnce you have done that please make a username via this [link](https://www.minecraft.net/en-us/msaprofile/mygames/editprofile) and try play again, once you have done that please try play again.\n\n*If your issue still persists please select the ``🔁 Restart`` below and click the other button.* ")
