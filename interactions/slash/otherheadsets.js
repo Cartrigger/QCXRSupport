e7cf5ddca4b3a2f530b40e6d1a06226789c73460
Cart <cartrigger@gmail.com>
2024-08-20 17:32:11 -0400

    updates

--- interactions/slash/otherheadsets.js
+++ interactions/slash/otherheadsets.js
@@ -19,13 +19,13 @@ module.exports = {
             .setDescription("YVR, a headset company from China, has expressed interest in having QuestCraft on their platform. We have finished developing YVRCraft in full. It is using V3 and V4 code.")
             .setColor("Green")
             .setFooter({
-                text: "Written by Cart, Updated by ChatGPT",
+                text: "Written by Cart",
                 iconURL: "https://images-ext-1.discordapp.net/external/aTyGSUOhnDS_8fX4GGP9UfztBpotA7axEqb0ujMNz0U/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/317814254336081930/a_89da6baf3d01206f8bd3e0f4ac91295f.gif?width=372&height=372"
             })
 
         const embed_otherheadsets = new EmbedBuilder()
             .setTitle("**QuestCraft on other Headsets**")
-            .setDescription("QuestCraft is planned to release on other headsets in the future, however, it is required that the company contacts us first in order to do this. \n Currently we are planned to release on **Pico** headsets in the future.")
+            .setDescription("QuestCraft is planned to release on other headsets in the future. \n Currently we are planned to release on **Pico** headsets in the future. From the recent past, we released and discontinued YVRCraft.")
             .setColor("Green")
 
         if (categories === "yvr") {
