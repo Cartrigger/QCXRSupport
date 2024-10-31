7ccfe3bb9be9cef82581429e67a7ee987d47a67d
Cart <81428538+cartrigger@users.noreply.github.com>
2024-10-30 22:28:39 -0400

    Guide for Crashing on pressing play, via markdown

---
+++ assets/guides/crashplay.md
@@ -0,0 +1,40 @@
+*Solution is for those that haven't added any extra mods than the default ones*
+
+❌  **You most likely got an interrupted download**
+*easiest way to solve this is to reinstall the instance*
+
+🟣 **Step by step**🟣
+**In instances**
+- Delete the instance
+- Create a new instance, make sure it is set to download default mods as well
+
+**Playscreen**
+- Choose that instance
+- Press play
+- Wait until the text in the lower right corner have disappeared
+-  Wait around 1 minute **after** it has disappeared
+- Then press play
+
+⚠️ **don’t remove your headset during the whole download**⚠️
+
+🟣 **Other things will stop the download:**🟣
+• letting the headset go idle
+• exiting your guardian
+• clicking any buttons in the app
+• going into passthrough
+• messing with wifi settings
+• quitting the app
+
+ℹ️ **Additional info**
+Average completion time is 2-20 minutes but it depends on your internet speed.
+• The file size is around 700 megabytes
+- 1.18 doesn't work
+
+- 1.19.4 is currently bugged.
+  *(It is fixed on the current __patreon build__ and will be fixed when 5.1.0 launch)*
+
+- Until release of 5.1.0
+  You can still play 1.19.4 **IF** you remove sodium.**
+  (Though you will get worse performance)
+
+*Alternatively, if you know how to build sodium yourself, you can do that. But we won’t assist with it*
--- interactions/slash/crashplay.js
+++ interactions/slash/crashplay.js
@@ -2,22 +2,22 @@ const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName("guide")
+        .setName("crashplay")
         .setDescription("Displays the guide from the markdown document."),
     async execute(interaction) {
         try {
-            const response = await fetch('https://raw.githubusercontent.com/Cartrigger/QCXRSupport/refs/heads/main/assets/guides/guide.md');
+            const response = await fetch('https://raw.githubusercontent.com/Cartrigger/QCXRSupport/refs/heads/main/assets/guides/crashplay.md');
             if (!response.ok) {
                 new Error(`HTTP error! Status: ${response.status}`);
             }
             const markdown = await response.text();
 
             const embed_guide = new EmbedBuilder()
-                .setTitle("**Guide**")
+                .setTitle("**My game crashes when I press play**")
                 .setDescription(markdown)
                 .setColor("Blue")
                 .setFooter({
-                    text: "Written by my Developers",
+                    text: "Written by my Support Staff",
                     iconURL: "https://images-ext-1.discordapp.net/external/5cPanAYF2wOpIhmzrCkina2UpawRtHHU4y2KjFviqCI/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/997670790604542012/92a28b5bb82c7512fe8711fa2fd63163.png?width=633&height=633"
                 });
 
