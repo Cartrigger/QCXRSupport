155831fd89e27fe918a4361829f4791f9c2c3c87
Cart <81428538+cartrigger@users.noreply.github.com>
2024-12-14 20:57:23 -0500

    +

--- interactions/slash/v71.js
+++ interactions/slash/v71.js
@@ -1,8 +1,8 @@
 const {EmbedBuilder, SlashCommandBuilder} = require("discord.js");
 
 const embed_v71 = new EmbedBuilder()
-    .setTitle("**Meta Quest V71 has major issues!**")
-    .setDescription("**Meta Quest Version 71 has major issues impacting QuestCraft!**\n\nThese issues usually include crashing without reason.\nThere is no known way of fixing this, so you need to wait till QuestCraft updates.\n\nYou can see more info in <#1242887330969157723>.")
+    .setTitle("**Meta Quest V71+ has major issues!**")
+    .setDescription("**Meta Quest Version 71+ has major issues impacting QuestCraft!**\n\nThese issues usually include crashing without reason.\nThere is no known way of fixing this, so you need to wait till QuestCraft updates.\nThere is no way to downgrade.\n\nYou can see more info in <#1242887330969157723>.")
     .setColor("Green")
     .setFooter({
         text: "Written by my Developers",
@@ -12,7 +12,7 @@ module.exports = {embed_v71}
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("v71")
-        .setDescription("Tells you about v71"),
+        .setDescription("Tells you about v71+"),
     async execute(interaction) {
         await interaction.reply({embeds: [embed_v71]});
     },
