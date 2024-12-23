3f9fc556984133acb821e001c2ae08e17083fb6c
Cart <81428538+Cartrigger@users.noreply.github.com>
2024-12-23 00:34:21 -0500

    Switch to switch statements, add XRCraft back, and fix pojav spelling.

--- interactions/slash/invites.js
+++ interactions/slash/invites.js
@@ -12,25 +12,27 @@ module.exports = {
                     {name: "QuestCraft Discord", value: "questcraft"},
                     {name: "Vivecraft Discord", value: "vivecraft"},
                     {name: "Vivecraft Mixin Discord", value: "vivecraft_mixin"},
-                    {name: "PojavLauncher Discord", value: "pojav_launcher"},
+                    {name: "PojavLauncher Discord", value: "pojavlauncher"},
+                    {name: "XRCraft Discord", value: "xrcraft"},
                 )),
     async execute(interaction) {
         const categories = interaction.options.getString("invites");
-        if (categories === "questcraft") {
-            await interaction.reply("https://discord.gg/questcraft");
-            return;
-        }
-        if (categories === "vivecraft") {
-            await interaction.reply("https://discord.com/invite/2x3QCk8qa9");
-            return;
-        }
-        if (categories === "vivecraft_mixin") {
-            await interaction.reply("https://discord.gg/bnatWRD8v3");
-            return;
-        }
-        if (categories === "pojav_launcher") {
-            await interaction.reply("https://discord.gg/pojavlauncher-724163890803638273");
-            return;
+        switch(categories){
+            case "xrcraft":
+                await interaction.reply("https://discord.gg/xrcraft-947244318740054149");
+                return;
+            case "pojavlauncher":
+                await interaction.reply("https://discord.gg/pojavlauncher-724163890803638273");
+                return;
+            case "vivecraft_mixin":
+                await interaction.reply("https://discord.gg/bnatWRD8v3");
+                return;
+            case "vivecraft":
+                await interaction.reply("https://discord.com/invite/2x3QCk8qa9");
+                return;
+            case "questcraft":
+                await interaction.reply("https://discord.gg/questcraft")
+                return;
         }
     }
 };
