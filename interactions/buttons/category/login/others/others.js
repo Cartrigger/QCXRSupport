9482b2cf58bd6ee246dc8581def7f84f2da85555
Cart <81428538+Cartrigger@users.noreply.github.com>
2024-12-25 19:13:20 -0500

    HOW??????? WHY DID IT NOT WORK BEFORE???? THIS DODESN"T MKE ANY SENSE!?!?!??!?!!

--- interactions/buttons/category/login/others/others.js
+++ interactions/buttons/category/login/others/others.js
@@ -1,6 +1,15 @@
 const login_embeds = require("../login_embeds");
 const buttons = require("../restart/restart_buttons");
-const embed_adb = require("../../../../slash/adb")
+const {EmbedBuilder} = require("discord.js");
+const embed_adb = new EmbedBuilder()
+    .setTitle("**Uninstalling using ADB commands**")
+    .setDescription("If you are having trouble uninstalling by normal means and it requires a force uninstall because it is not showing up, you need to make sure you are using **[SideQuest Advanced](https://SideQuestvr.com/setup-howto)** as basic SideQuest wont work. Run the command: ``adb uninstall com.qcxr.qcxr``, as shown in the image below.")
+    .setImage("https://media.discordapp.net/attachments/1059913125308145716/1111366186274390226/ADB.png?width=643&height=670")
+    .setColor("Green")
+    .setFooter({
+        text: "Written by my Developers",
+        iconURL: "https://images-ext-1.discordapp.net/external/5cPanAYF2wOpIhmzrCkina2UpawRtHHU4y2KjFviqCI/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/997670790604542012/92a28b5bb82c7512fe8711fa2fd63163.png?width=633&height=633"
+    });
 module.exports = {
     id: "others",
 
