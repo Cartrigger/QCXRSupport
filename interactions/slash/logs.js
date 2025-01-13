cdd3bba0c94d42ce9cab4fe6176ef9039ceb9cb1
Cart <81428538+cartrigger@users.noreply.github.com>
2025-01-13 02:59:58 -0500

    Move away from Discord CDN usage in logs.

--- interactions/slash/logs.js
+++ interactions/slash/logs.js
@@ -20,7 +20,7 @@ module.exports = {
             .setTitle("**Pc Logs Guide**")
             .setDescription("**Sending a QuestCraft Log on PC:**\n\n1) Connect your headset to your pc via a cable that supports data transfer.\n2) Go into your headset and accept the file access prompt.\n3) Look onto your pc and find the new device that was recognized in windows explorer.\n4) On the device go to android/data/com.qcxr.qcxr/files/latestlog.txt.\n5) Once you found this file, drag it into a different folder on your pc, then drag it into discord.\n\n**Note: if there was no file access prompt, you may have file access prompts disabled in headset developer settings. If that doesn't work, try using a different cable.**")
             .setColor("Green")
-            .setImage("https://media.discordapp.net/attachments/1057074981135196230/1099818983911346277/stinkers.gif?width=900&height=507")
+            .setImage("https://raw.githubusercontent.com/Cartrigger/QCXRSupport/refs/heads/main/assets/pc_logs.gif")
             .setFooter({
                 text: "Written by !DED",
                 iconURL: "https://media.discordapp.net/attachments/1057074981135196230/1096581536834404483/f2b05cbf99d0acf7f1da063f57b0053e.png?width=318&height=318"
@@ -29,7 +29,7 @@ module.exports = {
         const embed_quest_logs = new EmbedBuilder()
             .setTitle("**Getting logs on Quest Guide**")
             .setDescription("**Sending logs from Quest:**\n\n Restart your game, press on need help on the bottom right, then post the log link inside of support, or ticket where the person is asking for logs.")
-            .setImage("https://cdn.discordapp.com/attachments/821076673331724309/1243279697459150959/image.png?ex=6650e5f2&is=664f9472&hm=4dff3409b8b444a11e294f4395024cadd5f0da1c81d5001c72f8523f4d8a7cda&")
+            .setImage("https://raw.githubusercontent.com/Cartrigger/QCXRSupport/refs/heads/main/assets/quest_logs.png")
             .setColor("DarkOrange")
             .setFooter({
                 text: "Written by Cart",
