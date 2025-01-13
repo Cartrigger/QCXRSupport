4f5c99e4d7eab99351280807eae646e3e85e669b
Cart <81428538+cartrigger@users.noreply.github.com>
2025-01-13 03:09:20 -0500

    im going to sleep after this probably or else ill do more commits

--- interactions/slash/fixed.js
+++ interactions/slash/fixed.js
@@ -2,7 +2,7 @@ const {EmbedBuilder, SlashCommandBuilder} = require("discord.js");
 
 const embed_fixed = new EmbedBuilder()
     .setTitle("**Was QuestCraft fixed...?**")
-    .setDescription("Yes! on <t:1671995040:D>!\nQuest 3 was fixed on <t:1716507120>!")
+    .setDescription("Yes! on <t:1671995040:D>!\nQuest 3 was fixed on <t:1716507120>\nV71+ Has not been fixed yet")
     .setColor("Green")
 module.exports = {embed_fixed};
 module.exports = {
