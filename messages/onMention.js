a4c8ea7c53924bbbfe296871e6b4779224d75cb1
Cart <81428538+cartrigger@users.noreply.github.com>
2025-01-15 21:48:35 -0500

    make the @ ping more useful for owners of the bot

--- messages/onMention.js
+++ messages/onMention.js
@@ -1,8 +1,12 @@
 const {EmbedBuilder} = require("discord.js");
-const {prefix} = require("../config.json");
+const {owner} = require("../config.json");
 
 module.exports = {
     async execute(message) {
+        if (message.author === owner) {
+            const mention = new EmbedBuilder()
+                .setDescription(`Hey ${message.author}! I am online and my ping is \`\`${message.client.ws.ping}ms\`\`.`)
+        }
         const mention = new EmbedBuilder()
             .setDescription(`Hi ${message.author}! I am Crafty! To see my full list of commands, run \`/help\``)
             .setColor("Gold");
