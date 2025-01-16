d3ae5ed8e75b29fe1f0678cf8638d37b48906148
Cart <81428538+cartrigger@users.noreply.github.com>
2025-01-15 21:51:53 -0500

    well now it works

--- messages/onMention.js
+++ messages/onMention.js
@@ -5,11 +5,13 @@ module.exports = {
     async execute(message) {
         if (message.author === owner) {
             const mention = new EmbedBuilder()
-                .setDescription(`Hey ${message.author}! I am online and my ping is \`\`${message.client.ws.ping}ms\`\`.`)
-        }
-        const mention = new EmbedBuilder()
+                .setDescription(`Hey ${message.author}! I am online and my ping is \`\`${message.client.ws.ping}ms\`\`.`);
+            return message.reply({embeds: [mention]});
+        } else {
+            const mention = new EmbedBuilder()
             .setDescription(`Hi ${message.author}! I am Crafty! To see my full list of commands, run \`/help\``)
             .setColor("Gold");
-        return message.reply({embeds: [mention]});
+            return message.reply({embeds: [mention]});
+        }
     }
 };
