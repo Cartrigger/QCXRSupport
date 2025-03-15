0beaae1bcb6220a4ac62e3eb7e52910314f6310d
Cart <81428538+Cartrigger@users.noreply.github.com>
2025-03-15 16:51:15 -0400

    properly implement loginquick

--- interactions/slash/loginquick.js
+++ interactions/slash/loginquick.js
@@ -2,8 +2,8 @@ const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName("guide")
-        .setDescription("Displays the guide from the markdown document."),
+        .setName("loginquick")
+        .setDescription("Displays the login guide from the markdown document."),
     async execute(interaction) {
         try {
             const response = await fetch('https://raw.githubusercontent.com/Cartrigger/QCXRSupport/refs/heads/main/assets/guides/loginquick.md');
@@ -17,7 +17,7 @@ module.exports = {
                 .setDescription(markdown)
                 .setColor("Blue")
                 .setFooter({
-                    text: "Written by my Developers",
+                    text: "Written by our support members",
                     iconURL: "https://images-ext-1.discordapp.net/external/5cPanAYF2wOpIhmzrCkina2UpawRtHHU4y2KjFviqCI/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/997670790604542012/92a28b5bb82c7512fe8711fa2fd63163.png?width=633&height=633"
                 });
 
