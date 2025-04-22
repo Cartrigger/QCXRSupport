1dfe82b4b94740d8ed5c510b8a0bcdd23a05e7fd
Cart <81428538+cartrigger@users.noreply.github.com>
2025-04-22 04:10:33 -0400

    rewrite some wording

--- interactions/slash/renderers.js
+++ interactions/slash/renderers.js
@@ -19,25 +19,25 @@ module.exports = {
 
         const embed_renderer_gl4es = new EmbedBuilder()
             .setTitle("**QuestCraft GL4ES Renderer**")
-            .setDescription("QuestCraft versions 3.1.2 and below utilize the **GL4ES renderer**, it has moderate speed with moderate mod compatibility.\n- Complex mods like [Create](https://www.curseforge.com/minecraft/mc-mods/create-fabric) do not work.\n\nPlease note moving forwards, on QuestCraft versions 4.x, GL4ES is replaced with Zink.")
+            .setDescription("QuestCraft versions 3.x and below utilize the **Holy GL4ES renderer**, it has moderate speed with moderate mod compatibility.\n\nDerived from PojavLauncher, it was the best option available.\n\nComplex mods like [Create](https://modrinth.com/mod/create-fabric) do not work properly.")
             .setColor("Orange")
             .setFooter({
-                text: "Written by my Developers and my Contributors",
-                iconURL: "https://media.discordapp.net/attachments/1078126187064332359/1098737664754192545/IMG_8202.png?width=633&height=633"
+                text: "Written by Cart and my Contributors",
+                iconURL: "https://cdn.discordapp.com/avatars/317814254336081930/a_89da6baf3d01206f8bd3e0f4ac91295f.gif?size=4096"
             })
 
         const embed_renderer_zink = new EmbedBuilder()
             .setTitle("**QuestCraft Zink Renderer**")
-            .setDescription("QuestCraft versions 4.x utilize the **Zink Renderer**. It is moderately fast. \nThe Zink is a renderer implemented by the QuestCraft team to optimise the rendering of QuestCraft, but also improve mod compatibility.\n\nThis means that more complex mods, like [Create](https://modrinth.com/mod/create-fabric) do work in QC!")
+            .setDescription("QuestCraft versions 4.x utilize the **Zink Renderer**. It is moderately fast. \nThe Zink is a renderer implemented by the QuestCraft team to optimize the rendering of QuestCraft, but also improve mod compatibility.\n\nThis means that more complex mods, like [Create](https://modrinth.com/mod/create-fabric) work with this renderer.\n\nZink was removed from QuestCraft due to the performance degradation and a better renderer.")
             .setColor("Orange")
             .setFooter({
-                text: "Written by JaffaJace and my Contributors",
+                text: "Written by Cart and Jaffa",
                 iconURL: "https://images-ext-1.discordapp.net/external/CNDjGEZxdZHTQQnmL43jjbUyilF1FNrpbwl4dzh1xhY/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/654048865137459261/ec3ef49d087775eb2c26b10a4dc7c456.png?width=539&height=539"
             })
 
         const embed_renderer_regal = new EmbedBuilder()
             .setTitle("**QuestCraft Regal Renderer**")
-            .setDescription("QuestCraft v5.0 is using Regal renderer, it allows most mods to work and gives extremely high FPS, but Iris does not work in this case.")
+            .setDescription("QuestCraft v5.0 is using Regal renderer, it allows most mods to work and gives high FPS, but Iris does not work in this case.")
             .setColor("Orange")
             .setFooter({
                 text: "Written by Cart",
