8fd6c07d06459a906671b46b1dd842f6e8012f0b
Cart <81428538+Cartrigger@users.noreply.github.com>
2025-06-06 11:29:51 -0400

    Update automatic

--- triggers/notices/supportchannels.js
+++ triggers/notices/supportchannels.js
@@ -6,7 +6,7 @@ module.exports = {
         const channelExceptions = ["945502948111290498", "946021441298649158", "1057074981135196230", "821078174992957480", "821076673331724309", "932673625813823518"];
         const embed_support_channels = new EmbedBuilder()
             .setTitle("**Sorry but <:notsupportchannel:993043214472859778>**")
-            .setDescription("Support is handled in <#945502948111290498> or <#946021441298649158> and you can see the FAQ in <#821469293156892724>.\nOn top of this, you can see common issues in <#1242887330969157723>\n\nFor more guided support, you can make a ticket in <#946184706486054984>, and a support member will be with you shortly.")
+            .setDescription("Support is handled in <#945502948111290498> or <#946021441298649158> and you can see the FAQ in <#1378035608605167636>.\nOn top of this, you can see common issues in <#1378034717793714186>\n\nFor more guided support, you can make a ticket in <#946184706486054984>, and a support member will be with you shortly.")
             .setColor("Green")
         if (channelExceptions.includes(message.channelId) || channelExceptions.includes(message.channel.parentId)) {
             return;
