a6a2d6eabf2527aeadd162f878541ce3869ddd13
Cart <81428538+cartrigger@users.noreply.github.com>
2025-06-25 22:41:40 -0400

    blehhh

--- interactions/slash/versions.js
+++ interactions/slash/versions.js
@@ -2,7 +2,7 @@ const {EmbedBuilder, SlashCommandBuilder} = require("discord.js");
 
 const embed_versions = new EmbedBuilder()
     .setTitle("**Version Info**")
-    .setDescription("QuestCraft currently supports Minecraft versions:\n> 1.19.2 | 1.19.4 | 1.20.1 | 1.20.4 | 1.20.6 | 1.21.1 | 1.21.4 \n\nYou are unable to use any versions below 1.19.2\nTo change versions, click the version number for a dropdown, or create a new instance with a version of choice.")
+    .setDescription("QuestCraft currently supports Minecraft versions:\n> 1.19.2 | 1.19.4 | 1.20.1 | 1.20.4 | 1.20.6 | 1.21.1 | 1.21.4 | 1.21.5 \n\nYou are unable to use any versions below 1.19.2\nTo change versions, click the version number for a dropdown, or create a new instance with a version of choice.")
     .setColor("Green")
     .setImage("https://github.com/Cartrigger/QCXRSupport/blob/main/assets/versions.jpg?raw=true")
     .setFooter({
